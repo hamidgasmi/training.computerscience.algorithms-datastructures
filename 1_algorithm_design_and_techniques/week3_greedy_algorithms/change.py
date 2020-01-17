b8b7749042911a59685e53a3f5e3e048770f8cbe
Hamid Gasmi <mamid1706@hotmail.fr>
2020-01-17 14:57:18 -0800

    Issue #1 solved

--- 1_algorithm_design_and_techniques/week3_greedy_algorithms/change.py
+++ 1_algorithm_design_and_techniques/week3_greedy_algorithms/change.py
@@ -1,11 +1,17 @@
 # Greedy Algorithms project - Issue #1
 import sys
 
-#
-
+# Greedy Strategy:
+### Make a greedy choice: To choose the biggest coin (Cmax) that is small to m 1st as a safe choice
+### Prove that it is a safe choice:
+### Reduce to a subproblem: remove the biggest coin
+### Solve the subproblem (Iterate): Solve the subproblem with the rest of division m and without the biggest coin 
+# O(1)
 def get_change(m):
-    #write your code here
-    return m
+    n10 = m // 10
+    n5 = (m % 10) // 5
+    n1 = m - 10 * n10 - 5 * n5
+    return n10 + n5 + n1
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
