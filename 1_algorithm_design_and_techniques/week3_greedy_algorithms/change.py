464b6fb1eaf6921fde46716c8f41216b727ed5a5
Hamid Gasmi <mamid1706@hotmail.fr>
2020-01-17 13:45:10 -0800

    Greedy Algorithm description is added

---
+++ 1_algorithm_design_and_techniques/week3_greedy_algorithms/change.py
@@ -0,0 +1,12 @@
+# Greedy Algorithms project - Issue #1
+import sys
+
+#
+
+def get_change(m):
+    #write your code here
+    return m
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
