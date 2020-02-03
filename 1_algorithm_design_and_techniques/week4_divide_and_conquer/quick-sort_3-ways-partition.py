092d1bc1be76a1336b9cbd1157ba569c87a980df
Hamid Gasmi <mamid1706@hotmail.fr>
2020-02-02 16:36:36 -0800

    Issue #9: 1st version of 3-way partition quick sorted is added

--- 1_algorithm_design_and_techniques/week4_divide_and_conquer/quick-sort_3-ways-partition.py
+++ 1_algorithm_design_and_techniques/week4_divide_and_conquer/quick-sort_3-ways-partition.py
@@ -1,26 +1,19 @@
 import sys
 import random
 
-def partition2(a, l, r):
-    x = a[l]
-    j = l
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-
+def partition3(a, l, r):
+    m = [0] * 2
+    return m
 
 def randomized_quick_sort(a, l, r):
     if l >= r:
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
-
+   
+    m = partition3(a, l, r)
+    randomized_quick_sort(a, l, m[0] - 1)
+    randomized_quick_sort(a, m[1] + 1, r)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
