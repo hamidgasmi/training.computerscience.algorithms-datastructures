b67b6e509d7379f152e87c49c127deee99fb287c
Hamid Gasmi <mamid1706@hotmail.fr>
2020-02-10 17:20:38 -0800

    Issue #16 Longest Common Subsequence of Two Sequences is done

--- 1_algorithm_design_and_techniques/week5_and_6_dynamic_programming/lcs2.py
+++ 1_algorithm_design_and_techniques/week5_and_6_dynamic_programming/lcs2.py
@@ -1,10 +1,26 @@
 #Uses python3
-
 import sys
 
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    
+    D = [[0 for c in range(len(a) + 1)] for r in range(len(b) + 1)] 
+    for c in range(1, len(a) + 1):
+        D[0][c] = 0
+    for r in range(1, len(b) + 1):
+        D[r][0] = 0
+
+    for c in range(1, len(a) + 1):
+        for r in range(1, len(b) + 1):
+            
+            if a[c - 1] == b[r - 1]:
+                D[r][c] = max(D[r - 1][c - 1] + 1, D[r][c - 1], D[r - 1][c])
+            else:
+                D[r][c] = max(D[r - 1][c - 1], D[r][c - 1], D[r - 1][c])
+
+    for r in range(len(b) + 1):
+        print(D[r])
+
+    return D[len(b)][len(a)]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,3 +36,8 @@ if __name__ == '__main__':
     b = data[:m]
 
     print(lcs2(a, b))
+
+#python3 lcs2.py <<< "3 2 7 5 2 2 5"
+#python3 lcs2.py <<< "1 7 4 1 2 3 4"
+#python3 lcs2.py <<< "4 2 7 8 3 4 5 2 8 7"
+
