95167f61bc9c8818e4b337938820760d697968fa
Hamid Gasmi <mamid1706@hotmail.fr>
2020-02-22 17:36:49 -0800

    Issue #32 (Merging tables) is updated

--- 2-data-sructures-fundamentals/3_priority_queues_and_disjoint_sets/merging_tables.py
+++ 2-data-sructures-fundamentals/3_priority_queues_and_disjoint_sets/merging_tables.py
@@ -3,7 +3,6 @@ class Database:
         self.row_counts = row_counts
         self.max_row_count = max(row_counts)
         n_tables = len(row_counts)
-        self.ranks = [1] * n_tables
         self.parents = list(range(n_tables))
 
     def merge(self, src, dst):
@@ -16,10 +15,19 @@ class Database:
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
+        self.parents[src_parent] = dst_parent
+        self.row_counts[dst_parent] +=  self.row_counts[src_parent]
+        self.row_counts[src_parent] = 0
+        if self.max_row_count < self.row_counts[dst_parent]:
+            self.max_row_count = self.row_counts[dst_parent]
+        
         return True
 
     def get_parent(self, table):
         # find parent and compress path
+        if table != self.parents[table]:
+            self.parents[table] = self.get_parent(self.parents[table])
+
         return self.parents[table]
 
 
@@ -28,11 +36,13 @@ def main():
     counts = list(map(int, input().split()))
     assert len(counts) == n_tables
     db = Database(counts)
+
     for i in range(n_queries):
         dst, src = map(int, input().split())
-        db.merge(dst - 1, src - 1)
+        db.merge(src - 1, dst - 1)
         print(db.max_row_count)
-
+        #print("Parents:", db.parents)
+        #print("Size:", db.row_counts)
 
 if __name__ == "__main__":
     main()
