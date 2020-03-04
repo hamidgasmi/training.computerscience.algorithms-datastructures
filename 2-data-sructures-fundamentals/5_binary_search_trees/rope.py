3ca33733478bb33909f431ddcc5b373d10d15af8
Hamid Gasmi <mamid1706@hotmail.fr>
2020-03-04 15:41:40 -0800

    Issue #75 (Rope) started

--- 2-data-sructures-fundamentals/5_binary_search_trees/rope.py
+++ 2-data-sructures-fundamentals/5_binary_search_trees/rope.py
@@ -1,153 +1,177 @@
 # python3
 import sys
 
+# Splay tree implementation
+
+# Vertex of a splay tree
 class Vertex:
-  def __init__(self, key, index, left, right, parent):
-      self.key = key
-      self.index = index
-      self.left = left
-      self.right = right
-      self.parent = parent
 
-  def grandParent(self):
-      if self.parent == None:
-          return None
-      else:
-          return self.parent.parent
+  def __init__(self, key, left, right, parent):
+    (self.key, self.left, self.right, self.parent) = (key, left, right, parent)
+    self.rank = 1
+    self.size = 1
 
+  def str(self):
+      return "(key, rank, size, left, right) = (" + str(self.key) + "," + str(self.rank) + ","  + str(self.size) + "," + \
+      ("None" if self.left == None else str(self.left.key)) + "," + \
+      ("None" if self.right == None else str(self.right.key)) + ")"
+      
   def update(self):
+      self.size = 1 + (self.left.size if self.left != None else 0) \
+                    + (self.right.size if self.right != None else 0)
+      self.rank = 1 + (self.left.size if self.left != None else 0)
+
       if self.left != None:
           self.left.parent = self
-      
+
       if self.right != None:
           self.right.parent = self
+
+  def smallRotation(self):
+      parent = self.parent
+      if parent == None:
+          return
+      grandparent = self.parent.parent
+      if parent.left == self:
+          m = self.right
+          self.right = parent
+          parent.left = m
+      else:
+          m = self.left
+          self.left = parent
+          parent.right = m
+      parent.update()
+      self.update()
+      self.parent = grandparent
+      if grandparent != None:
+          if grandparent.left == parent:
+              grandparent.left = self
+          else: 
+              grandparent.right = self
+          grandparent.update()
+
+  def bigRotation(self):
+      if self.parent.left == self and self.parent.parent.left == self.parent:
+        # Zig-zig
+        self.parent.smallRotation()
+        self.smallRotation()
+      elif self.parent.right == self and self.parent.parent.right == self.parent:
+        # Zig-zig
+        self.parent.smallRotation()
+        self.smallRotation()
+      else: 
+        # Zig-zag
+        self.smallRotation()
+        self.smallRotation()
+
 
 class Rope:
   def __init__(self, s):
       self.s = s
       self.root = None
       for i in range(len(s)):
-          self.insert(i, s[i])
+          self.insert(i)
   
   def update(self, v):
       if v == None:
           return
       else:
           v.update()
-
-  def smallRotation(self, v):
-      if v == None:
-          return
-    
-      parent = v.parent()
-      if parent == None:
-          return
-
-      grandparent = v.grandParent()
-      if parent.left == v:
-          m = v.right
-          v.right = parent
-          parent.left = m
-      else:
-          m = v.left()
-          v.left = parent
-          parent.right = m
-      parent.update()
-      v.update()
-
-      v.parent = grandparent
-      if grandparent != None:
-          if grandparent.left == parent:
-              grandparent.left = v
-          else: 
-              grandparent.right = v
-
-  def bigRotation(self, v):
-      if v == None:
-          return
-          
-      p = v.parent()
-      q = v.grandParent()
-      if p == None or q == None:
-          return
-      
-      if (p == q.left() and v == p.left()) or \
-         (p == q.right() and v == q.right()):
-         # Zig-Zig
-          self.smallRotation(p)
-          self.smallRotation(v)
-      elif (p == q.left() and v == q.right() or 
-            p == q.right() and v == p.left()):
-          # Zig-Zag
-          self.smallRotation(v)
-          self.smallRotation(v)
-
+  
+  def insert(self, i):
+      new_vertex = Vertex(i, None, None, None)
+      if self.root != None:
+          new_vertex.left = self.root
+          new_vertex.update()
+      self.root = new_vertex
+  
   def splay(self, v):
       if v == None:
-        return None
-
+          return None
       while v.parent != None:
-        if v.parent.parent == None:
-            self.smallRotation(v)
-            break
-        self.bigRotation(v)
-
+          if v.parent.parent == None:
+              v.smallRotation()
+              break
+          v.bigRotation()
       return v
 
-  def find(self, key): 
-      v = self.root
-      last = self.root
+  def find(self, root, index): 
+      v = root
+      last = root
       next = None
       while v != None:
-        if v.key >= key and (next == None or v.key < next.key):
-            next = v    
-        last = v
-        if v.key == key:
+          if v.rank >= index and (next == None or v.rank < next.rank):
+              next = v    
+          last = v
+          if v.rank == index:
             break    
-        if v.key < key:
+          if v.rank < index:
+            index -= v.rank
             v = v.right
-        else: 
-            v = v.left  
-      self.root = self.splay(last)
-      return (next)
+            
+          else: 
+              v = v.left      
+      root = self.splay(last)
+      return (next, root)
 
-  def split(self, v, key):
-      pass
-  
-  def split(self, key):  
-      result = self.find(key)  
+  def split(self, root, index):  
+      (result, root) = self.find(root, index)
       if result == None:    
-          return (self.root, None)
-  
+        return (root, None)
+
       right = self.splay(result)
       left = right.left
       right.left = None
       if left != None:
           left.parent = None
-      self.update(left)
-      self.update(right)
+          left.update()
+      right.update()
       return (left, right)
+  
+  def merge(self, left, right):
+      if left == None:
+          return right
+      if right == None:
+          return left
+      while right.left != None:
+          right = right.left
+      right = self.splay(right)
+      right.left = left
+      right.update()
+      return right
+  
+  def printRope(self, v, new_s):
+      if v == None:
+          return
+      
+      if v.left != None:
+          self.printRope(v.left, new_s)
+      
+      new_s.append(self.s[v.key])
 
-  def merge(self, v1, v2):
-      pass
-
-  def insert(self, i, c):
-      (left, right) = self.split(self.root, i)
-      new_vertex = None
-      if right == None or right.key != i:
-          new_vertex = Vertex(i, i, None, None, None)  
-      self.root = merge(merge(left, new_vertex), right)
+      if v.right != None:
+          self.printRope(v.right, new_s)
 
   def result(self):
-      return self.s
+      new_s = []
+      self.printRope(self.root, new_s)
+
+      new_str = ""
+      return new_str.join(new_s)
 
   def process(self, i, j, k):
-      pass
 
-if __name__ == '__main__':         
+      (left, middle) = self.split(self.root, i + 1)
+      (middle, right) = self.split(middle, j - i + 2)
+      self.root = self.merge(left, right)
+      (left, right) = self.split(self.root, k + 1)
+      self.root = self.merge(left, middle)
+      self.root = self.merge(self.root, right)
+      
+if __name__ == '__main__':
     rope = Rope(sys.stdin.readline().strip())
     q = int(sys.stdin.readline())
     for _ in range(q):
         i, j, k = map(int, sys.stdin.readline().strip().split())
         rope.process(i, j, k)
-    print(rope.result())
+        print(rope.result())
