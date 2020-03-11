40e39322451fdb9bee47eac4799a10364b5503c1
Hamid Gasmi <mamid1706@hotmail.fr>
2020-03-11 07:20:30 -0700

    Issue 87 (Finding an Exit from a Maze): solved with a Graph DFS exploration

--- 3-graph-algorithms/1_graph_decomposition/reachability.py
+++ 3-graph-algorithms/1_graph_decomposition/reachability.py
@@ -1,10 +1,63 @@
-#Uses python3
-
 import sys
+from queue import Queue 
 
-def reach(adj, x, y):
-    #write your code here
-    return 0
+class MazeGraph:
+    def __init__(self, n, adj):
+        self.adj = adj
+        self.visited = [False] * n
+
+    def visit(self, v):
+        self.visited[v] = True
+
+    def isVisited(self, v):
+        return self.visited[v]
+
+    # Time Complexity: O(|V|)
+    # Space Complexity: O(|V|)
+    # Good job! (Max time used: 0.03/5.00, max memory used: 9535488/536870912.)
+    def explore_dfs(self, u, v):
+        if u == v:
+            return 1
+
+        self.visit(u)
+        for i in range(len(adj[u])):
+            if not self.isVisited(adj[u][i]):
+                if self.explore_dfs(adj[u][i], v) == 1:
+                    return 1
+
+        return 0
+
+    # Time Complexity: O(|V|)
+    # Space Complexity: O(|V|)
+    def explore_bfs(self, u, v, q):
+        if u == v:
+            return 1
+
+        q.put(u)
+        while not q.empty():
+            u = q.get()
+            if u == v:
+                return 1
+            self.visit(u)
+
+            for i in range(len(adj[u])):
+                if not self.isVisited(adj[u][i]):
+                    q.put(adj[u][i])
+               
+        return 0
+
+def reach(adj, n, u, v, solution):
+
+    aMaze = MazeGraph(n, adj)
+    if solution == 1:
+        return aMaze.explore_dfs(u, v) 
+    elif solution == 2:
+        q = Queue(maxsize = n) 
+        return aMaze.explore_bfs(u, v, q)
+    elif solution == 3:
+        return 0
+    else:
+        return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -18,4 +71,5 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+
+    print(reach(adj, n, x, y, 2))
