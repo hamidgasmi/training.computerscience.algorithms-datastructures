d6788b8423d5b675a4833f2306616b8464d72720
Hamid Gasmi <mamid1706@hotmail.fr>
2020-03-12 13:02:17 -0700

    Issue #88 is completed with a BFS algorithm

--- 3-graph-algorithms/1_graph_decomposition/connected_components.py
+++ 3-graph-algorithms/1_graph_decomposition/connected_components.py
@@ -1,12 +1,50 @@
 #Uses python3
-
 import sys
 
+class MazeGraph:
+    def __init__(self, n, edges):
+        self.adj = [[] for _ in range(n)]
+        self.buildAdjacencyList(edges)
+        self.visited = [False] * n
 
-def number_of_components(adj):
-    result = 0
-    #write your code here
-    return result
+    # Time Complexity: O(|E|)
+    # Space Complexity: O(1)
+    def buildAdjacencyList(self, edges):
+        for (a, b) in edges:
+            self.adj[a - 1].append(b - 1)
+            self.adj[b - 1].append(a - 1)
+
+    def isVisited(self, v):
+        return self.visited[v]
+
+    # Time Complexity: O(|E|)
+    # Space Complexity: O(|V|):
+    # .... Each vertex calls recursively explore method for 1 neighbor at a time
+    def explore(self, v):
+        self.visited[v] = True
+        for a in range(len(self.adj[v])):
+            if not self.isVisited(self.adj[v][a]):
+                self.explore(self.adj[v][a])
+
+    # Time Complexity: O(|V| + |E|):
+    # .... Each vertex is explored exactly once
+    # .... Each vertex checks each neighbor
+    # Space Complexity: O(|V|):
+
+    def DFS(self):
+        
+        connected_components = 0
+        for v in range(len(self.adj)):
+            if not self.visited[v]:
+                connected_components += 1
+                self.explore(v)
+        return connected_components
+
+def number_of_components(n, edges):
+
+    aMaze = MazeGraph(n, edges)
+
+    return aMaze.DFS()
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -14,8 +52,5 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(number_of_components(adj))
+    
+    print(number_of_components(n, edges))
