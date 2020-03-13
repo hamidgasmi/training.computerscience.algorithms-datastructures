eb62db95170c969988ccc7dd4a46a2a78b47a4f7
Hamid Gasmi <mamid1706@hotmail.fr>
2020-03-13 02:24:59 -0700

    Issue 96 is completed with a DFS

--- 3-graph-algorithms/1_graph_decomposition/toposort.py
+++ 3-graph-algorithms/1_graph_decomposition/toposort.py
@@ -1,17 +1,42 @@
 #Uses python3
-
 import sys
 
-def dfs(adj, used, order, x):
-    #write your code here
-    pass
+class Graph:
+    def __init__(self, n, edges):
+        self.adj = [[] for _ in range(n)]
+        self.buildAdjacencyList(edges)
+    
+    # Time Complexity: O(|E|)
+    # Space Complexity: O(1)
+    def buildAdjacencyList(self, edges):
+        self.reset()
 
+        for (a, b) in edges:
+            self.adj[a - 1].append(b - 1)
 
-def toposort(adj):
-    used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+    def reset(self):
+        self.visited = [False] * len(self.adj)
+        self.removed = [False] * len(self.adj)
+
+    def explore(self, v, order):
+        self.visited[v] = True
+        for a in self.adj[v]:
+            if not self.removed[a]:
+                self.explore(a, order)
+        
+        self.removed[v] = True
+        order.append(v)
+
+    def dfs(self, order):
+        self.reset()
+        for v in range(len(self.adj)):
+            if not self.removed[v]:
+                self.explore(v, order)
+
+    def toposort(self):
+        order = []
+        self.dfs(order)
+        return order[::-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -19,10 +44,9 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-    order = toposort(adj)
+    
+    aGraph = Graph(n, edges)
+    order = aGraph.toposort()
     for x in order:
         print(x + 1, end=' ')
 
