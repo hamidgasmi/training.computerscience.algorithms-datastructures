07cf63b0de47daab5cb85c9ab405a0a2d1782b10
Hamid Gasmi <mamid1706@hotmail.fr>
2020-03-16 22:16:04 -0700

    Issue #132 is completed

--- 3-graph-algorithms/2_paths_in_graphs/bipartite.py
+++ 3-graph-algorithms/2_paths_in_graphs/bipartite.py
@@ -1,11 +1,45 @@
 #Uses python3
-
 import sys
 import queue
 
-def bipartite(adj):
-    #write your code here
-    return -1
+class Graph:
+    def __init__(self, n, edges):
+        self.adj = [[] for _ in range(n)]
+        self.buildAdjacencyList(edges)
+
+    # Time Complexity: O(|E|)
+    # Space Complexity: O(1)
+    def buildAdjacencyList(self, edges):
+        for (a, b) in edges:
+            self.adj[a - 1].append(b - 1)
+            self.adj[b - 1].append(a - 1)
+
+    # Time Complexity: O(|V| + |E|)
+    # Space Complexity: O(|V|)
+    def bfs(self):
+
+        color = [-1] * len(self.adj)
+        color[0] = 0
+
+        q = queue.Queue()
+        q.put(0)
+        while not q.empty():
+            v = q.get()
+            otherColor = 0 if color[v] == 1 else 1
+            for a in self.adj[v]:
+                if color[a] == -1:
+                    color[a] = otherColor
+                    q.put(a)
+                elif color[a] == color[v]:
+                    return 0
+
+        return 1
+
+
+def bipartite(n, edges):
+    aGraph = Graph(n, edges)
+    
+    return aGraph.bfs()
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -13,8 +47,4 @@ if __name__ == '__main__':
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(bipartite(adj))
+    print(bipartite(n, edges))
