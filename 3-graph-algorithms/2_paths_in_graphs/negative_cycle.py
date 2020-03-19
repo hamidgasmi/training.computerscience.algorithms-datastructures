bb25c3ba1d41038e8313328a1354ad3cc73213bc
Hamid Gasmi <mamid1706@hotmail.fr>
2020-03-18 20:16:02 -0700

    Issue #134 is completed by using Belman-Ford Algorithm

--- 3-graph-algorithms/2_paths_in_graphs/negative_cycle.py
+++ 3-graph-algorithms/2_paths_in_graphs/negative_cycle.py
@@ -1,12 +1,43 @@
 #Uses python3
-
 import sys
+from collections import namedtuple
 
+Edge = namedtuple('Edge', ['vertex', 'weight'])
 
-def negative_cycle(adj, cost):
-    #write your code here
-    return 0
+class Graph:
+    def __init__(self, n, edges):
+        self.adjacency_list = [[] for _ in range(n)]
+        self.build_adjacency_list(edges)
 
+    def size(self):
+        return len(self.adjacency_list)
+
+    def build_adjacency_list(self, edges):
+        for ((s, d), w) in edges:
+            self.adjacency_list[s - 1].append(Edge(d - 1, w))
+
+    def bellman_ford(self):
+        distances = [10**7 + 1] * self.size()
+        parents = [-1] * self.size()
+
+        distances[0] = 0
+
+        for i in range(self.size()):
+            for v in range(self.size()):
+                for a in self.adjacency_list[v]:
+                    a_candidate_distance = distances[v] + a.weight
+                    if distances[a.vertex] > a_candidate_distance:
+                        if i == self.size() - 1:
+                            return 1
+
+                        distances[a.vertex] = a_candidate_distance
+                        parents[a.vertex] = v
+        
+        return 0
+
+def negative_cycle(n, edges):
+    
+    return Graph(n, edges).bellman_ford()
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -15,9 +46,4 @@ if __name__ == '__main__':
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
     data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
-    for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+    print(negative_cycle(n, edges))
