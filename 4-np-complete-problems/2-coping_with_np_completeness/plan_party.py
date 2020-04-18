a4829d5c7aca1d848862c49934ce4d4e4738b53e
Hamid Gasmi <mamid1706@hotmail.fr>
2020-04-17 20:44:53 -0700

    Issue #153 is started

--- 4-np-complete-problems/2-coping_with_np_completeness/plan_party.py
+++ 4-np-complete-problems/2-coping_with_np_completeness/plan_party.py
@@ -1,28 +1,28 @@
 #uses python3
-
 import sys
 import threading
 
-# This code is used to avoid stack overflow issues
-sys.setrecursionlimit(10**6) # max depth of recursion
-threading.stack_size(2**26)  # new thread will get stack of such size
-
+# This problem can be reduced to Independent Set problem (ISP) which is an NP problem
+# It's a special case of ISP 
+# It's can be solved effeciently with Dynamic Programming technique:
+# Let's W(v) reprensents the Total Fun (weight) in the party, then:
+# F(v) = Max(weight(v) + sum(weight(u) for all u grand-children of v),
+#                        sum(weight(u) for all u children of v))
 
 class Vertex:
     def __init__(self, weight):
         self.weight = weight
         self.children = []
 
+class Tree:
+    def __init__(self, size, weights):
+        self.size = size
+        self.weights = [weights[i] for i in range(size)]
 
+        
 def ReadTree():
-    size = int(input())
-    tree = [Vertex(w) for w in map(int, input().split())]
-    for i in range(1, size):
-        a, b = list(map(int, input().split()))
-        tree[a - 1].children.append(b - 1)
-        tree[b - 1].children.append(a - 1)
-    return tree
-
+    
+    
 
 def dfs(tree, vertex, parent):
     for child in tree[vertex].children:
@@ -44,10 +44,25 @@ def MaxWeightIndependentTreeSubset(tree):
 
 
 def main():
-    tree = ReadTree();
-    weight = MaxWeightIndependentTreeSubset(tree);
+    tree = ReadTree()
+    weight = MaxWeightIndependentTreeSubset(tree)
     print(weight)
 
+if __name__ == "__main__":
+    
+    # To avoid stack overflow issues
+    sys.setrecursionlimit(10**6) # max depth of recursion
+    threading.stack_size(2**26)  # new thread will get stack of such size
 
-# This is to avoid stack overflow issues
-threading.Thread(target=main).start()
+    size = int(input())
+    weights = map(int, input().split())
+    print(weights)
+    #tree = [Vertex(w) for w in map(int, input().split())]
+    #for i in range(1, size):
+    #    a, b = list(map(int, input().split()))
+    #    tree[a - 1].children.append(b - 1)
+    #    tree[b - 1].children.append(a - 1)
+    #return tree
+
+    # This is to avoid stack overflow issues
+    #threading.Thread(target=main).start()
