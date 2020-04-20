a06ac3d04ddb9a648441c75115eb4a261791ca5d
Hamid Gasmi <mamid1706@hotmail.fr>
2020-04-19 20:23:40 -0700

    Issues #140 updated

--- 4-np-complete-problems/1-np_complete_problems/budget_allocation.py
+++ 4-np-complete-problems/1-np_complete_problems/budget_allocation.py
@@ -9,35 +9,41 @@ from sys import stdin
 # For each inequality, find all vectors X (X1, X2, X3) that invalidate the inequality:
 # ... There're 8 different vectors X
 # ... Add a clause for each invalidating vector
-def add_inequality_invalidating_clauses(a, b, clauses):
+def add_inequality_invalidating_clauses(a, b, clauses, clause_set):
   
   len_a = len(a)
   for combination in itertools.product(range(2), repeat=len_a):
     
+    clause_num = 0
+    clause = []
     inequality_left_part = 0
     for i in range(len_a):
       inequality_left_part += a[i] * combination[i]
 
-    if inequality_left_part > b:
-      clauses.append([])
-      for i in range(len_a):
-        clauses[len(clauses) - 1].append(i+1 if combination[i] == 0 else -i-1)
-      if (len(clauses) == 2):
-        print("a: ", a)
-        print("combination: ", combination)
-        print("clauses[1]", clauses[1])
+      Xi = i+1 if combination[i] == 0 else -i-1
+      clause.append(Xi)
+      clause_num *= 10 
+      clause_num += (Xi + 4)
+
+    if inequality_left_part > b and not clause_num in clause_set:
+        clauses.append(clause)
+        clause_set.add(clause_num)
 
 def build_clauses(A, b, clauses):
 
+    clause_set = set()
+    clause_num = 0
     # 1. Adding clauses corresponding to possible values for Xi: Xi V -Xi
     clauses.append([])
     for Xi in range(1, len(A[0]) + 1):
         clauses[0].append(Xi)
-        clauses.append([-Xi, Xi])
+        clause_num *= 10 
+        clause_num += (Xi + 4)
+    clause_set.add(clause_num)
     
     # 2. Adding Clauses corresponding to values that invalidate each inequality
     for i in range(len(b)):
-      add_inequality_invalidating_clauses(A[i], b[i], clauses)
+      add_inequality_invalidating_clauses(A[i], b[i], clauses, clause_set)
   
 def printEquisatisfiableSatFormula(clauses, var_nbr):
 
