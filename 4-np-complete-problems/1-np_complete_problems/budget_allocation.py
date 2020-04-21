13206b9bdf73f6921ac8a9e75a7d564a14afe29f
Hamid Gasmi <mamid1706@hotmail.fr>
2020-04-20 20:36:21 -0700

    #140 is completed

--- 4-np-complete-problems/1-np_complete_problems/budget_allocation.py
+++ 4-np-complete-problems/1-np_complete_problems/budget_allocation.py
@@ -2,51 +2,60 @@
 import itertools
 from sys import stdin
 
-
 # Analysis:
-# Let Xi a boolean variable for i in { 1, 2, 3 }:
-# At least one variable must be selected: (X1 V X2 V X3)
+# Let Xi a boolean variable for i in { 1, 2, 3 }
 # For each inequality, find all vectors X (X1, X2, X3) that invalidate the inequality:
 # ... There're 8 different vectors X
 # ... Add a clause for each invalidating vector
 # ... If 0 0 0 is invalidating the inequality, (X1, X2, X3) shound't be equal to (0, 0, 0)
 # ... It's equivalent to: X1 V X2 V X3
-def add_inequality_invalidating_clauses(a, b, clauses, clause_set):
-  
-  len_a = len(a)
-  for combination in itertools.product(range(2), repeat=len_a):
-    
-    clause_num = 0
-    clause = []
-    inequality_left_part = 0
-    for i in range(len_a):
-      inequality_left_part += a[i] * combination[i]
-
-      Xi = i+1 if combination[i] == 0 else -i-1
-      clause.append(Xi)
-      clause_num *= 10 
-      clause_num += (Xi + 4)
-
-    if inequality_left_part > b and not clause_num in clause_set:
-        clauses.append(clause)
-        clause_set.add(clause_num)
-
 def build_clauses(A, b, clauses):
 
-    clause_set = set()
-    clause_num = 0
-    # 1. Adding clauses corresponding to possible values for Xi: Xi V -Xi
-    clauses.append([])
-    for Xi in range(1, len(A[0]) + 1):
-        clauses[0].append(Xi)
-        clause_num *= 10 
-        clause_num += (Xi + 4)
-    clause_set.add(clause_num)
+    unsatisfiable = False  
+    var_nbr = len(A[0])
+    # Clause: Xi Values that are invalidating an inequality must be excluded
+    combination_nbr = 2 ** var_nbr
+    for combination in itertools.product(range(2), repeat=var_nbr):
+
+      clause = []
+      for i in range(var_nbr):
+        clause.append(i+1 if combination[i] == 0 else -i-1)
+
+      for i in range(len(b)):
+
+        a = A[i]
+
+        unsatisfiable = True if b[i] < 0 else False
+        inequality_left_part = 0
+        for j in range(var_nbr):
+          inequality_left_part += a[j] * combination[j]
+          unsatisfiable = unsatisfiable if a[j] == 0 else False
+          
+        if unsatisfiable:
+          break
+        
+        if inequality_left_part > b[i]:
+          clauses.append(clause)
+          break
+      
+      if len(clauses) == combination_nbr:
+        unsatisfiable = True
+
+      if unsatisfiable:
+        break
     
-    # 2. Adding Clauses corresponding to values that invalidate each inequality
-    for i in range(len(b)):
-      add_inequality_invalidating_clauses(A[i], b[i], clauses, clause_set)
-  
+    if unsatisfiable:
+      clauses.clear()
+      clauses.append([1])
+      clauses.append([-1])
+      var_nbr = 1
+      
+    elif len(clauses) == 0:
+      clauses.append([1, -1])
+      var_nbr = 1
+    
+    return var_nbr
+
 def printEquisatisfiableSatFormula(clauses, var_nbr):
 
   print(len(clauses), var_nbr)
@@ -65,6 +74,6 @@ if __name__ == "__main__":
   b = list(map(int, stdin.readline().split()))
 
   clauses = []
-  build_clauses(A, b, clauses)
+  var_nbr = build_clauses(A, b, clauses)
 
   printEquisatisfiableSatFormula(clauses, var_nbr)
