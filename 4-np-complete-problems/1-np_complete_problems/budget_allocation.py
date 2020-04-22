16fffcfb8b22ecbcb13f11fa9667e229d16bb1e6
Hamid Gasmi <mamid1706@hotmail.fr>
2020-04-21 18:12:24 -0700

    #140 is refactored and completed

--- 4-np-complete-problems/1-np_complete_problems/budget_allocation.py
+++ 4-np-complete-problems/1-np_complete_problems/budget_allocation.py
@@ -1,60 +1,68 @@
+#Uses python3
 import itertools
 from sys import stdin
 
 # Analysis:
-# Let Xi a boolean variable for i in { 1, 2, 3 }
-# For each inequality, find all vectors X (X1, X2, X3) that invalidate the inequality:
-# ... There're 8 different vectors X
+# Let Xi a boolean variable for i in { 1, 2, 3, ..., n }
+# For each inequality, find all vectors X (Xi, Xj, Xk) that invalidate the inequality:
 # ... Add a clause for each invalidating vector
 # ... If 0 0 0 is invalidating the inequality, (X1, X2, X3) shound't be equal to (0, 0, 0)
 # ... It's equivalent to: X1 V X2 V X3
 # Special Cases:
 # ... If all coefficient are equal to 0 for an inequality and b < 0, then there is no solution
 # ... If the formula reaches 8 cases, then there is no solution
-def build_clauses(A, b, clauses):
-
-    unsatisfiable = False  
-    var_nbr = len(A[0])
-    combination_nbr = 2 ** var_nbr
-
+def build_clauses(A, b, formula):
+  
     # Clause: Xi Values that are invalidating an inequality must be excluded
-    for combination in itertools.product(range(2), repeat=var_nbr):
+    unsatisfiable = False
+    var_nbr = 0
+    var_no_map = [0] * len(A[0])
+    for i in range(len(b)):
 
-      clause = []
-      for i in range(var_nbr):
-        clause.append(i+1 if combination[i] == 0 else -i-1)
-
-      for i in range(len(b)):
-
-        a = A[i]
-
-        unsatisfiable = True if b[i] < 0 else False
-        inequality_left_part = 0
-        for j in range(var_nbr):
-          inequality_left_part += a[j] * combination[j]
-          unsatisfiable = unsatisfiable if a[j] == 0 else False
+        a = []
+        ineq_var = []
+        for j in range(len(A[i])):
+          if A[i][j] == 0:
+            continue
           
-        if unsatisfiable:
-          break
-        
-        if inequality_left_part > b[i]:
-          clauses.append(clause)
-          break
-      
-      if len(clauses) == combination_nbr:
-        unsatisfiable = True
+          a.append(A[i][j])
+          # Find the variable No
+          if var_no_map[j] == 0:
+            var_nbr += 1
+            var_no_map[j] = var_nbr
+          ineq_var.append(var_no_map[j])
+          
+        ineq_var_nbr = len(a) # <= 3
 
-      if unsatisfiable:
-        break
-    
+        unsatisfiable = False
+        if ineq_var_nbr == 0 and b[i] < 0:
+          unsatisfiable = True
+          break
+        elif ineq_var_nbr == 0:
+          continue
+
+        for combination in itertools.product(range(2), repeat=ineq_var_nbr):
+          
+          clause = []
+          inequality_left_part = 0
+          for j in range(ineq_var_nbr):
+            inequality_left_part += a[j] * combination[j]
+            clause.append(ineq_var[j] if combination[j] == 0 else -ineq_var[j])
+          
+          if inequality_left_part > b[i]:
+            formula.append(clause)
+
+    if len(formula) >= 2**var_nbr:
+      unsatisfiable = True
+
     if unsatisfiable:
-      clauses.clear()
-      clauses.append([1])
-      clauses.append([-1])
+      formula.clear()
+      formula.append([1])
+      formula.append([-1])
       var_nbr = 1
       
-    elif len(clauses) == 0:
-      clauses.append([1, -1])
+    elif len(formula) == 0:
+      formula.append([1, -1])
       var_nbr = 1
     
     return var_nbr
