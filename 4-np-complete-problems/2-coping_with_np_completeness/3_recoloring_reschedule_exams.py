a84fc07264e7c43a005b0249e2ebd1e61b99e41e
Hamid Gasmi <mamid1706@hotmail.fr>
2020-04-23 21:23:34 -0700

    #146 new test cases are added

--- 4-np-complete-problems/2-coping_with_np_completeness/3_recoloring_reschedule_exams.py
+++ 4-np-complete-problems/2-coping_with_np_completeness/3_recoloring_reschedule_exams.py
@@ -1,40 +1,32 @@
 # python3
 import sat2_circuit_design
 
-# Arguments:
-#   * `n` - the number of vertices.
-#   * `edges` - list of edges, each edge is a tuple (u, v), 1 <= u, v <= n.
-#   * `colors` - list consisting of `n` characters, each belonging to the set {'R', 'G', 'B'}.
-# Return value: 
-#   * If there exists a proper recoloring, return value is a list containing new colors, similar to the `colors` argument.
-#   * Otherwise, return value is None.
 class Reduct_3_coloring_to_2_sat:
 
     def __init__(self, n, edges, colors):
-        
+
+        self.color_num = dict()
+        self.color_num["R"] = 1
+        self.color_num["G"] = 2
+        self.color_num["B"] = 3
         self.f_convert_3_coloring_imputs_to_2_sat(n, edges, colors)
         self.implication_graph = sat2_circuit_design.Implication_graph(n * 3, self.clauses)
 
-    def color_num(self, color):
-        
-        return 1 if color == 'R' else 2 if color == 'G' else 3
-
     def sat_v_num(self, v_no, color):
-
-        return 3 * (v_no - 1) + self.color_num(color)
+        return 3 * (v_no - 1) + self.color_num[color]
 
     def f_convert_3_coloring_imputs_to_2_sat(self, n, edges, colors):
         self.clauses = []
         
         # Clauses 1: vertices possible colors
         # Clauses 2: a vertice can't have 2 different colors at the same time 
-        # Clauses 2: vertices color must be changed
+        # Clauses 3: vertices color must be changed
         for i in range(n):
             possible_colors_clause = []
             exactly_one_clause = []
-            for c in range(3):
-                if colors[0][i] == "RGB"[c % 3]:
-                    self.clauses.append([- self.sat_v_num(i+1, colors[0][i])])
+            for c in range(0, 3):
+                if colors[i] == "RGB"[c % 3]:
+                    self.clauses.append([- self.sat_v_num(i+1, colors[i])])
                 else:
                     X = self.sat_v_num(i+1, "RGB"[c % 3])
                     possible_colors_clause.append(X)
@@ -43,14 +35,13 @@ class Reduct_3_coloring_to_2_sat:
             self.clauses.append(possible_colors_clause)
             self.clauses.append(exactly_one_clause)
         
-        # Clauses 2: Each vertice color must be different to all adjacent vertices color
+        # Clauses 4: Each vertice color must be different to all adjacent vertices color
         for (u, v) in edges:
             
-            for c in range(3):
-                adjacent_vertices_clauses = []
-                adjacent_vertices_clauses.append(- self.sat_v_num(u, "RGB"[c % 3]))
-                adjacent_vertices_clauses.append(- self.sat_v_num(v, "RGB"[c % 3]))
-                self.clauses.append(adjacent_vertices_clauses)
+            for c in range(0, 3):
+                if "RGB"[c % 3] in (colors[u - 1], colors[v - 1]):
+                    continue
+                self.clauses.append([ -self.sat_v_num(u, "RGB"[c % 3]), -self.sat_v_num(v, "RGB"[c % 3])])
 
     def solve_2_sat(self):
         self.sat_solution = self.implication_graph.solve_2sat(True)
@@ -62,10 +53,10 @@ class Reduct_3_coloring_to_2_sat:
 
         new_colors = []
         #print(self.clauses)
-        #print(self.sat_solution)
+        print(self.sat_solution)
         for i in range(len(self.sat_solution)):
             if self.sat_solution[i] == 1:
-                new_colors.append("RBG"[(i)% 3])
+                new_colors.append("BRG"[(i+1)%3])
 
         return new_colors
  
@@ -77,11 +68,12 @@ def assign_new_colors(n, edges, colors):
     
 def main():
     n, m = map(int, input().split())
-    colors = input().split()
+    colors = input()
     edges = []
     for _ in range(m):
         u, v = map(int, input().split())
         edges.append((u, v))
+
     new_colors = assign_new_colors(n, edges, colors)
     if new_colors is None:
         print("Impossible")
