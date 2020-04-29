c179c64969a704fde0c8a1f426a910f6f570f55e
Hamid Gasmi <mamid1706@hotmail.fr>
2020-04-28 18:47:15 -0700

    #156 is completed with a Trie

--- 5-string-processing-and-pattern-matching-algorithms/1-suffix-trees/trie_matching_extended.py
+++ 5-string-processing-and-pattern-matching-algorithms/1-suffix-trees/trie_matching_extended.py
@@ -1,25 +1,111 @@
 import sys
 
-NA = -1
+# Class to store Trie(Patterns)
+# It handles all cases particularly the case where a pattern Pi is a subtext of a pattern Pj for i != j
+class Trie_patterns:
+    def __init__(self, patterns):
+        self.build_trie(patterns)
 
-class Node:
-	def __init__ (self):
-		self.next = [NA] * 4
-		self.patternEnd = False
+    def search_text(self, pattern, index):
+        if len(self.trie) == 0:
+            return (0, -1)
 
-def solve (text, n, patterns):
-	result = []
+        node = 0
+        i = index
+        while i < len(pattern):
 
-	# write your code here
+            c = pattern[i]
 
-	return result
+            if pattern[i] in self.trie[node]:
+                node = self.trie[node][c]
+                i += 1
+                continue
 
-text = sys.stdin.readline ().strip ()
-n = int (sys.stdin.readline ().strip ())
-patterns = []
-for i in range (n):
-	patterns += [sys.stdin.readline ().strip ()]
+            else:
+                break
+        
+        return (i, node)
 
-ans = solve (text, n, patterns)
+    def insert(self, pattern):
 
-sys.stdout.write (' '.join (map (str, ans)) + '\n')
+        (index, node) = self.search_text(pattern, 0)
+        if index == len(pattern):
+			# the text is already in the Trie
+            return
+        
+        for i in range(index, len(pattern)):
+            c = pattern[i]
+            self.max_node_no += 1
+            self.trie[node][c] = self.max_node_no
+            self.trie[self.max_node_no] = dict()
+            node = self.max_node_no
+
+    # The trie will be a dictionary of dictionaries where:
+    # ... The key of the external dictionary is the node ID (integer), 
+    # ... The internal dictionary:
+    # ...... It contains all the trie edges outgoing from the corresponding node
+    # ...... Its keys are the letters on those edges
+    # ...... Its values are the node IDs to which these edges lead
+    # Time Complexity: O(|patterns|)
+    # Space Complexity: O(|patterns|)
+    def build_trie(self, patterns):
+                
+        self.trie = dict()
+        self.trie[0] = dict()
+        self.max_node_no = 0
+        for pattern in patterns:
+            self.insert(pattern + '$') # to handle the case where Pi is a substring of Pj for i != j
+    
+    # Prints the trie in the form of a dictionary of dictionaries
+    # E.g. For the following patterns: ["AC", "T"] {0:{'A':1,'T':2},1:{'C':3}}
+    def print_tree(self):
+        for node in self.trie:
+            for c in self.trie[node]:
+                print("{}->{}:{}".format(node, self.trie[node][c], c))
+
+    def search_in_pattern(self, text, index):
+        if len(self.trie) == 0:
+            return False
+
+        node = 0
+        text_index = index
+        while text_index < len(text):
+
+            c = text[text_index]
+            if not c in self.trie[node]:
+                return False
+
+            node = self.trie[node][c]
+            if '$' in self.trie[node]:
+                return True
+            else:
+                text_index += 1
+        
+        return False
+
+    # Time Complexity: O(|text| * |longest pattern|)
+    def multi_pattern_matching(self, text):
+        
+        if len(self.trie) == 0:
+            return []
+
+        maching_position = []
+        for i in range(len(text)):
+
+            if self.search_in_pattern(text, i):
+                maching_position.append(i)
+            
+        return maching_position
+
+if __name__ == "__main__":
+	
+	text = sys.stdin.readline().strip()
+	n = int(sys.stdin.readline().strip())
+	
+	patterns = []
+	for i in range (n):
+		patterns += [sys.stdin.readline().strip()]
+
+	trie = Trie_patterns(patterns)
+	ans = trie.multi_pattern_matching(text)
+	sys.stdout.write (' '.join (map (str, ans)) + '\n')
