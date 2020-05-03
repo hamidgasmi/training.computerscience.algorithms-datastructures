d1c1cd318165437f585602dda85ae0a4df1a235f
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-02 19:52:37 -0700

    #158 is completed

--- 5-string-processing-and-pattern-matching-algorithms/1-suffix-trees/non_shared_substring.py
+++ 5-string-processing-and-pattern-matching-algorithms/1-suffix-trees/non_shared_substring.py
@@ -1,55 +1,59 @@
 # python3
 import sys
 import suffix_tree
-import queue
 from collections import namedtuple
+import threading
 
-Edge = namedtuple('Edge', ['parent', 'node', 'start', 'len'])
-max_len = 2001
+# DFS traversal on the tree
+def dfs_non_shared_substring(st, node):
 
-# BFS traversal on the tree
-def dfs_non_shared_substring(suffix_tree, node):
-
-	if len(suffix_tree.nodes[node]) == 0:
-		return (node, 0, False)
+	if len(st.nodes[node]) == 0:
+		return (-1, 0, False)
 	
 	text_len = (len(suffix_tree.text) - 2) // 2
-	shortest_substr_start = 0
-	shortest_substr_len = max_len
+	shortest_substr_end = 0
+	shortest_substr_len = 2001
 	right_part = False
-	for child in suffix_tree.nodes[node]:
+	for child in st.nodes[node]:
 		
-		if suffix_tree.nodes[node][child].start >= text_len:
-			right_part = True if suffix_tree.nodes[node][child].start > text_len else right_part
+		if st.nodes[node][child].start >= text_len:
+			right_part = True if st.nodes[node][child].start > text_len else right_part
 			continue
 		
-		(substr_start, substr_len, subright_part) = dfs_non_shared_substring(suffix_tree, child)
+		(substr_end, substr_len, subright_part) = dfs_non_shared_substring(suffix_tree, child)
 		right_part = True if subright_part else right_part
+		
+		candidate_len = 1 if substr_len == 0 else st.nodes[node][child].len + substr_len
+		candidate_end = st.nodes[node][child].start if substr_end == -1 else substr_end
 
-		if shortest_substr_len > suffix_tree.nodes[node][child].len + substr_len:
-			shortest_substr_start = suffix_tree.nodes[node][child].start
-			shortest_substr_len = 1 if substr_len == 0 else suffix_tree.nodes[node][child].len + substr_len
+		if shortest_substr_len > candidate_len:
+			shortest_substr_end = candidate_end
+			shortest_substr_len = candidate_len
 	
 	if not right_part:
+		shortest_substr_end = -1
 		shortest_substr_len = 0
-	elif shortest_substr_len == 0:
-		shortest_substr_len = 1
 
-	return (shortest_substr_start, shortest_substr_len, right_part)
+	return (shortest_substr_end, shortest_substr_len, right_part)
 
 def solve (p, q):
 	pq_text = p + "#" + q + "$"
-	
-	suffixTree = suffix_tree.Suffix_Tree(pq_text)
-	#print(suffixTree.nodes)
 
-	(shortest_substr_start, shortest_substr_len, right_part) = dfs_non_shared_substring(suffixTree, 0)
-	
-	return suffixTree.text[ shortest_substr_start : shortest_substr_start + shortest_substr_len ]
+	st = suffix_tree.Suffix_Tree(pq_text)
+	(shortest_substr_end, shortest_substr_len, right_part) = dfs_non_shared_substring(st, 0)
 
-if __name__ == "__main__":
+	return st.text[ shortest_substr_end -  shortest_substr_len + 1 : shortest_substr_end + 1 ]
+
+def main():
 	p = sys.stdin.readline ().strip ()
 	q = sys.stdin.readline ().strip ()
 
 	ans = solve (p, q)
 	sys.stdout.write (ans + '\n')
+
+if __name__ == "__main__":
+
+	sys.setrecursionlimit(10**7)
+	threading.stack_size(2**30)
+	threading.Thread(target=main()).start()
+
