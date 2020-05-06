494e7b4d18c98ce6f1d54db020ed835290ad3368
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-06 15:57:22 -0700

    #162 is optimized with a Count_Sort

--- 5-string-processing-and-pattern-matching-algorithms/2-burrows-wheeler-suffix-arrays/bwtinverse.py
+++ 5-string-processing-and-pattern-matching-algorithms/2-burrows-wheeler-suffix-arrays/bwtinverse.py
@@ -2,25 +2,56 @@
 import sys
 
 class Burrows_Wheeler_Inverse:
+    def __init__(self):
+        self.alphabet = dict()
+        self.alphabet["$"] = 0
+        self.alphabet["A"] = 1
+        self.alphabet["C"] = 2
+        self.alphabet["G"] = 3
+        self.alphabet["T"] = 4
 
+    # O(|text|)
     def inverse(self, bwt_text):
 
+        # O(|text|)
         last_column = [c for c in bwt_text]
+
+        # O(|text|)
         first_column = [c for c in bwt_text]
-        first_column.sort()
+        self.count_sort(first_column)
+
+        # O(|text|)
         last_to_first = self.build_last_to_first(first_column, last_column)
 
         origin_text_list = []
         origin_text_list.append(first_column[0])
 
+        # O(|text|)
         i = 0
         while last_to_first[i] != 0:
             i = last_to_first[i]
             origin_text_list.append(first_column[i])
 
+        # O(|text|)
         origin_text_list.reverse()
 
+        # O(|text|)
         return ''.join(origin_text_list)
+
+    # O(Text)
+    def count_sort(self, char_list):
+        count = [0 for _ in range(len(self.alphabet))]
+
+        for c in char_list:
+            i = self.alphabet[c]
+            count[i] += 1
+        
+        i = 0
+        for c in self.alphabet:
+            index = self.alphabet[c]
+            for _ in range(count[index]):
+                char_list[i] = c
+                i += 1
 
     def build_last_to_first(self, first_column, last_column):
         last_to_first = []
