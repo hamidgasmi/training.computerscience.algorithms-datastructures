7ce7639b23d8e8d59fac4c9b4914aeafcd4fb9b8
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-16 17:59:23 -0700

    #163 is completed with Knuth-Morris-Pratt algorithm

--- 5-string-processing-and-pattern-matching-algorithms/3-knuth-morris-pratt-algorithm/kmp.py
+++ 5-string-processing-and-pattern-matching-algorithms/3-knuth-morris-pratt-algorithm/kmp.py
@@ -1,21 +1,52 @@
-# python3
 import sys
 
+class Knuth_Morris_Pratt:
+  def __init__(self, text, pattern):
+    self.pattern = pattern
+    self.pattern_len = len(pattern)
+    self.text = text
 
-def find_pattern(pattern, text):
-  """
-  Find all the occurrences of the pattern in the text
-  and return a list of all positions in the text
-  where the pattern starts in the text.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+  def build_text(self):
 
+    text_list = [ self.pattern[i] for i in range(len(self.pattern)) ]
+    text_list.append("$")
+    for i in range(len(self.text)):
+      text_list.append(self.text[i])
+
+    return text_list
+
+  def compute_prefix(self, S):
+    
+    border = 0
+    prefix = [0 for _ in range(len(S))]
+    for i in range(1, len(S)):
+      while border > 0 and S[i] != S[border]:
+        border = prefix[border - 1]
+
+      if S[i] == S[border]:
+        border += 1
+      else:
+        border = 0
+      prefix[i] = border
+    
+    return prefix
+    
+  def find_pattern(self):
+
+    S = self.build_text()
+    prefix = self.compute_prefix(S)
+
+    result  = []
+    for i in range(self.pattern_len + 1, len(S), 1):
+      if prefix[i] == self.pattern_len:
+        result.append(i - 2 * self.pattern_len)
+
+    return result
 
 if __name__ == '__main__':
   pattern = sys.stdin.readline().strip()
   text = sys.stdin.readline().strip()
-  result = find_pattern(pattern, text)
-  print(" ".join(map(str, result)))
 
+  kmp = Knuth_Morris_Pratt(text, pattern)
+
+  print(" ".join(map(str, kmp.find_pattern())))
