6d48705b83985b39c944e7caa984f204a3778558
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-18 21:43:28 -0700

    #168 is completed with top-down appreach (memorization)

---
+++ 6-dynamic-programming-applications-in-machine-learning-and-genomics/1-sequence-alignment-1/change_coins_memorization.py
@@ -0,0 +1,48 @@
+#python3
+import sys
+
+# 1. Express a solution mathematically: Change(M) = min{ Change(M - Coin[i]) } + 1 for all i between 0 and |Coins| - 1
+# 2. Proof: Let's assume we came up with an optimal solution by using a subsolution j where Changes(j) > min{ change(j - Coin[i]) } + 1
+#           Change(M, j) = Change(j) + ...
+#           Let's note min{ change(Mj - Coin[i]) } + 1 = Change(j')
+#           But since Change(j) > Change(j')
+#           Then, we could replace in our optimal solution Change(j) by Change(j')
+#           This will lead us to a contradiction: We would build a new solution Change(M, j') < Change(M, j)
+# 3. Solutions: we can solve this problem in 2 ways:
+#    1. Bottom up solution
+#    2. Recursive + Memorization solution
+
+def find_change_memorized(m, coins, changes):
+    if changes[m] != sys.maxsize:
+        return changes[m]
+    
+    for coin in coins:
+        if changes[m] == -1 or m < coin:
+            continue
+        
+        candidate_change = find_change_memorized(m - coin, coins, changes)
+        if candidate_change == -1:
+            continue
+
+        changes[m] = min(changes[m], candidate_change + 1)
+
+    if changes[m] == sys.maxsize:
+        changes[m] = -1
+    
+    return changes[m]
+
+def find_change(monney, coins):
+    
+    changes = [sys.maxsize for i in range(money + 1)]
+    changes[0] = 0
+    for coin in coins:
+        if money >= coin:
+            changes[coin] = 1
+
+    return(find_change_memorized(monney, coins, changes))
+
+if __name__ == "__main__":
+    money = int(sys.stdin.readline().strip())
+    coins = list(map(int, sys.stdin.readline().strip().split(',')))
+    
+    print(find_change(money, coins))
