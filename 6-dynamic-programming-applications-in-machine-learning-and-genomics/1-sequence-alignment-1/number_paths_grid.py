3b128d92d01c941462f8a39834f9abc1958e5ce6
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-21 16:37:48 -0700

    #172 is completed with math combination solution

---
+++ 6-dynamic-programming-applications-in-machine-learning-and-genomics/1-sequence-alignment-1/number_paths_grid.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+# Solution: Combination = (total moves # choose total moves down #) or (total moves # choose total moves right #)
+# Running Time: O(n + m): linear
+# Space Complexity: O(1)
+def compute_number_paths_grid_combination(n, m):
+    
+    total_moves_nbr = (n - 1) + (m - 1)
+    total_moves_down = (n - 1)
+    return math.factorial(total_moves_nbr) // (math.factorial(total_moves_down) * math.factorial(total_moves_nbr - total_moves_down))
+
+# Solution: Naive Approach
+# Running Time: O(nm): Quadratic
+# Space Complexity: O(nm): Quadratic
+def compute_number_paths_grid(n, m):
+    grid = [ [1 for _ in range(m)] for _ in range(n)] 
+    grid[0][0] = 0
+        
+    for r in range(1, n, 1):
+        for c in range(1, m, 1):
+            grid[r][c] = grid[r][c - 1] + grid[r - 1][c]
+
+    return grid[n - 1][m - 1]
+
+if __name__ == "__main__":
+
+    n = int(sys.stdin.readline().strip())
+    m = int(sys.stdin.readline().strip())
+
+    #print(compute_number_paths_grid(n, m))
+    print(compute_number_paths_grid_combination(n, m))
+
+    
