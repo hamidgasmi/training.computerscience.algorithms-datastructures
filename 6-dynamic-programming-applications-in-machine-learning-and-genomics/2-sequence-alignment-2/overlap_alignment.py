3ae39d5208a858c5b1a093622e7183b06aef1674
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-23 20:00:20 -0700

    #176 is started

---
+++ 6-dynamic-programming-applications-in-machine-learning-and-genomics/2-sequence-alignment-2/overlap_alignment.py
@@ -0,0 +1,19 @@
+#python3
+import sys
+
+def align(m,mu,sigma,s,t):
+    """
+    m, mu, and sigma are the scoring parameters
+    s and t are the strings to be aligned
+    align a suffix of s with a prefix of t
+
+    return a string with the score and reconstructed alignment
+    be sure to follow the output format for this problem
+    """
+    # TODO: your code here
+    return ""
+
+if __name__ == "__main__":
+    m,mu,sigma = map(int,sys.stdin.readline().strip().split())
+    s,t = [sys.stdin.readline().strip() for _ in range(2)]
+    print(align(m,mu,sigma,s,t))
