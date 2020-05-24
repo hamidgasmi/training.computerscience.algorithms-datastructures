db6e6fc57c528be31675a35c41b1885097d19ce2
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-23 19:20:32 -0700

    #175 is completed: DP + buttom-up

--- 6-dynamic-programming-applications-in-machine-learning-and-genomics/2-sequence-alignment-2/fitting_alignment.py
+++ 6-dynamic-programming-applications-in-machine-learning-and-genomics/2-sequence-alignment-2/fitting_alignment.py
@@ -1,38 +1,102 @@
 import sys
-import global_alignment
+import math
 
-def align_naive(m,mu,sigma,s,t):
-    
-    alignment = global_alignment.Alignment(m, mu, sigma)
-    alignment.built_global_alignment_matrix(s, t)
-    max_sub_global_align_score = alignment.global_alignment_score
-    print("max_sub_global_align_score, i: ", 0, max_sub_global_align_score)
-    sub_s_start = 0
-    for i in range(1, len(s), 1):
-               
-        A = alignment.built_global_alignment_matrix(s[i:], t)
+# 1. Express a solution mathematically:
+#       Let's A be a matrix of alignments of (|s| + 1) x (|t| + 1)
+#       A[0,0] = 0
+#       A[0,c] = c * sigma for 1 <= c <= |s|
+#       A[r,0] = r * sigma for 1 <= r <= |t|
+#       A[1,c] = max(A[0,0] + match if s[c] = t[r] else mu, A[r-1,c-1] + match if s[c] = t[r] else mu, A[r-1,c] + sigma, A[r,c-1] + sigma) for 1 <= c <= |s| and r = 1
+#       A[r,c] = max(A[r-1,c-1] + match if s[c] = t[r] else mu, A[r-1,c] + sigma, A[r,c-1] + sigma) for 1 <= c <= |s| and 1 <= r <= |t|
+# 2. Proof:
+# 3. Implementation: buttom up solution
+#    Running time: O(nm)
+#    Space Complexity: O(nm)
+class Alignment:
+    def __init__(self, match, mu, sigma):
+        
+        self.match = match
+        self.mu = mu * (-1)
+        self.sigma = sigma * (-1)
+        self.fitting_alignment_score = 0
 
-        print("global_alignment_score, i: ", i, alignment.global_alignment_score)
-        if alignment.global_alignment_score > max_sub_global_align_score:
-            max_sub_global_align_score = alignment.global_alignment_score
-            sub_s_start = i
-            print("max_sub_global_align_score, i: ", i, alignment.global_alignment_score)
+    def global_alignment(self, s, t):
 
-    if sub_s_start == 0:
-        alignment.global_alignment(s, t)
-    else:
-        alignment.global_alignment(s[sub_s_start:], t)
-    
-    print(alignment.global_alignment_score)
-    print(alignment.aligned_seq_1)
-    print(alignment.aligned_seq_2)
+        A, max_alignment_r, max_alignment_c = self.built_fitting_alignment_matrix(s, t)
+        self.aligned_seq_1, self.aligned_seq_2 = self.get_fitting_alignment_backtrack(A, max_alignment_r, max_alignment_c)
 
-    #return ""
+        return self.fitting_alignment_score, self.aligned_seq_1, self.aligned_seq_2
+
+    def built_fitting_alignment_matrix(self, s, t):
+
+        rows_count = len(t) + 1
+        columns_count = len(s) + 1
+
+        A = []
+        A.append([c * self.sigma for c in range(columns_count)])
+        for r in range(1, rows_count, 1):
+            A.append([(r * self.sigma if c == 0 else 0) for c in range(columns_count)])
+        
+        max_alignment_r = rows_count - 1
+        max_alignment_c = 0
+        for r in range(1, rows_count, 1):
+            for c in range(1, columns_count, 1):
+                
+                alignment_score = A[r - 1][c - 1] + (self.match if s[c - 1] == t[r - 1] else self.mu)
+                delete_score = A[r][c-1] + self.sigma
+                insert_score = A[r-1][c] + self.sigma
+                A[r][c] = max((self.match if s[c - 1] == t[r - 1] else self.mu) if r == 1 else -math.inf, alignment_score, delete_score, insert_score)
+
+                if r == rows_count - 1 and A[r][c] > A[max_alignment_r][max_alignment_c]:
+                    max_alignment_r = r
+                    max_alignment_c = c
+
+        self.fitting_alignment_score = A[max_alignment_r][max_alignment_c]
+        #print(self.fitting_alignment_score)
+        #for r in range(rows_count):
+        #    print(A[r])
+
+        return A, max_alignment_r, max_alignment_c
+
+    def get_fitting_alignment_backtrack(self, A, max_alignment_r, max_alignment_c):
+        aligned_seq_1_inverse = []
+        aligned_seq_2_inverse = []
+
+        r = max_alignment_r
+        c = max_alignment_c
+        while r > 0 and c > 0:
+            
+            if r == 1 and c > 1 and A[r][c] == (self.match if s[c - 1] == t[r - 1] else self.mu):
+                aligned_seq_1_inverse.append(s[c - 1])
+                aligned_seq_2_inverse.append(t[r - 1])
+                c = 0
+                r = 0
+
+            elif A[r][c] == A[r - 1][c - 1] + (self.match if s[c - 1] == t[r - 1] else self.mu):
+                aligned_seq_1_inverse.append(s[c - 1])
+                aligned_seq_2_inverse.append(t[r - 1])
+                c -= 1
+                r -= 1
+
+            elif A[r][c] == A[r][c - 1] + self.sigma:
+                aligned_seq_1_inverse.append(s[c - 1])
+                aligned_seq_2_inverse.append('-')
+                c -= 1
+
+            elif A[r][c] == A[r - 1][c] + self.sigma:
+                aligned_seq_1_inverse.append('-')
+                aligned_seq_2_inverse.append(t[r - 1])
+                r -= 1
+        
+        return ''.join(aligned_seq_1_inverse[::-1]), ''.join(aligned_seq_2_inverse[::-1])
 
 if __name__ == "__main__":
     m,mu,sigma = map(int,sys.stdin.readline().strip().split())
     s,t = [sys.stdin.readline().strip() for _ in range(2)]
 
-    align_naive(m, mu, sigma, s, t)
-
-    #print(align(m,mu,sigma,s,t))
+    alignment = Alignment(m, mu, sigma)
+    alignment.global_alignment(s, t)
+    
+    print(alignment.fitting_alignment_score)
+    print(alignment.aligned_seq_1)
+    print(alignment.aligned_seq_2)
