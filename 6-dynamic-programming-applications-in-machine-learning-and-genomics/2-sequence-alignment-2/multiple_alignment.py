9196cc30b27b1975957aa8aad2e075afc18d8872
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-28 01:09:41 -0700

    #180 is completed: DP + Buttom-Up + Backtracking

--- 6-dynamic-programming-applications-in-machine-learning-and-genomics/2-sequence-alignment-2/multiple_alignment.py
+++ 6-dynamic-programming-applications-in-machine-learning-and-genomics/2-sequence-alignment-2/multiple_alignment.py
@@ -32,17 +32,9 @@ class Alignment:
         for row in range(1, rows_count, 1):
             for col in range(1, columns_count, 1):
                 for dep in range(1, depth_count, 1):
-            
-                    if r[row - 1] == s[col - 1] == t[dep - 1]:
-                        A[row][col][dep] = max(A[row - 1][col - 1][dep - 1] + 1,
-                                         A[row][col - 1][dep - 1], A[row][col - 1][dep], A[row][col][dep - 1],
-                                         A[row - 1][col][dep - 1], A[row - 1][col][dep], A[row - 1][col][dep - 1],
-                                         A[row - 1][col - 1][dep], A[row - 1][col][dep], A[row - 1][col - 1][dep])
-                    else:
-                        A[row][col][dep] = max(A[row - 1][col - 1][dep - 1],
-                                         A[row][col - 1][dep - 1], A[row][col - 1][dep], A[row][col][dep - 1],
-                                         A[row - 1][col][dep - 1], A[row - 1][col][dep], A[row - 1][col][dep - 1],
-                                         A[row - 1][col - 1][dep], A[row - 1][col][dep], A[row - 1][col - 1][dep])
+                    A[row][col][dep] = max(A[row - 1][col - 1][dep - 1] + self.match if r[row - 1] == s[col - 1] == t[dep - 1] else self.mu,
+                                           max(A[row][col - 1][dep - 1], A[row][col - 1][dep], A[row][col][dep - 1], A[row - 1][col][dep - 1], 
+                                               A[row - 1][col][dep], A[row - 1][col - 1][dep], A[row - 1][col][dep]) + self.sigma)
         
         self.global_alignment_score = A[rows_count - 1][columns_count - 1][depth_count - 1]
         
@@ -53,14 +45,133 @@ class Alignment:
         aligned_seq_1_inverse = []
         aligned_seq_2_inverse = []
         aligned_seq_3_inverse = []
-        return ''.join(aligned_seq_1_inverse[::-1]), ''.join(aligned_seq_2_inverse[::-1]), ''.join(aligned_seq_3_inverse[::-1])
         
-        #row = len(r)
-        #col = len(s)
-        #dep = len(t)
-        
-        #return ''.join(aligned_seq_1_inverse[::-1]), ''.join(aligned_seq_2_inverse[::-1]), ''.join(aligned_seq_3_inverse[::-1])
+        row = len(r)
+        col = len(s)
+        dep = len(t)
 
+        while row > 0 and col > 0 and dep > 0:
+            if A[row][col][dep] == A[row - 1][col - 1][dep - 1] + (self.match if s[col - 1] == r[row - 1] == t[dep - 1] else self.mu):
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_2_inverse.append(s[col - 1])
+                aligned_seq_3_inverse.append(t[dep - 1])
+                col -= 1
+                row -= 1
+                dep -= 1
+
+            elif A[row][col][dep] == A[row][col - 1][dep - 1] + self.sigma:
+                aligned_seq_1_inverse.append('-')
+                aligned_seq_2_inverse.append(s[col - 1])
+                aligned_seq_3_inverse.append(t[dep - 1])
+                col -= 1
+                dep -= 1
+
+            elif A[row][col][dep] == A[row][col - 1][dep] + self.sigma:
+                aligned_seq_1_inverse.append('-')
+                aligned_seq_2_inverse.append(s[col - 1])
+                aligned_seq_3_inverse.append('-')
+                col -= 1
+
+            elif A[row][col][dep] == A[row][col][dep - 1] + self.sigma:
+                aligned_seq_1_inverse.append('-')
+                aligned_seq_2_inverse.append('-')
+                aligned_seq_3_inverse.append(t[dep - 1])
+                dep -= 1
+
+            elif A[row][col][dep] == A[row - 1][col][dep - 1] + self.sigma:
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_2_inverse.append('-')
+                aligned_seq_3_inverse.append(t[dep - 1])
+                row -= 1
+                dep -= 1
+
+            elif A[row][col][dep] == A[row - 1][col][dep] + self.sigma:
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_2_inverse.append('-')
+                aligned_seq_3_inverse.append('-')
+                row -= 1
+
+            elif A[row][col][dep] == A[row - 1][col - 1][dep] + self.sigma:
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_2_inverse.append(s[col - 1])
+                aligned_seq_3_inverse.append('')
+                col -= 1
+                row -= 1
+        
+        while row > 0 and col > 0:
+            
+            if A[row][col][0] == A[row - 1][col - 1][0] + (self.match if s[col - 1] == r[row - 1] else self.mu):
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_2_inverse.append(s[col - 1])
+                col -= 1
+                row -= 1
+
+            elif A[row][col][0] == A[row][col - 1][0] + self.sigma:
+                aligned_seq_1_inverse.append('-')
+                aligned_seq_2_inverse.append(s[col - 1])
+                col -= 1
+
+            elif A[row][col][0] == A[row - 1][col][0] + self.sigma:
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_2_inverse.append('-')
+                row -= 1
+            aligned_seq_3_inverse.append('-')
+
+        while row > 0 and dep > 0:
+            if A[row][0][dep] == A[row - 1][0][dep - 1] + (self.match if t[dep - 1] == r[row - 1] else self.mu):
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_3_inverse.append(t[dep - 1])
+                dep -= 1
+                row -= 1
+
+            elif A[row][0][dep] == A[row][0][dep - 1] + self.sigma:
+                aligned_seq_1_inverse.append('-')
+                aligned_seq_3_inverse.append(t[dep - 1])
+                dep -= 1
+
+            elif A[row][0][dep] == A[row - 1][0][dep] + self.sigma:
+                aligned_seq_1_inverse.append(r[row - 1])
+                aligned_seq_3_inverse.append('-')
+                row -= 1
+            aligned_seq_2_inverse.append('-')
+
+        while col > 0 and dep > 0:
+            if A[0][col][dep] == A[0][col - 1][dep - 1] + (self.match if s[col - 1] == t[dep - 1] else self.mu):
+                aligned_seq_2_inverse.append(s[col - 1])
+                aligned_seq_3_inverse.append(t[dep - 1])
+                col -= 1
+                dep -= 1
+
+            elif A[0][col][dep] == A[0][col - 1][dep] + self.sigma:
+                aligned_seq_2_inverse.append(s[col - 1])
+                aligned_seq_3_inverse.append('-')
+                col -= 1
+
+            elif A[0][col][dep] == A[0][col][dep - 1] + self.sigma:
+                aligned_seq_2_inverse.append('-')
+                aligned_seq_3_inverse.append(t[dep - 1])
+                dep -= 1
+            aligned_seq_1_inverse.append('-')
+
+        while row > 0:
+            aligned_seq_1_inverse.append(r[row - 1])
+            aligned_seq_2_inverse.append('-')
+            aligned_seq_3_inverse.append('-')
+            row -= 1
+
+        while col > 0:
+            aligned_seq_1_inverse.append('-')
+            aligned_seq_2_inverse.append(s[col - 1])
+            aligned_seq_3_inverse.append('-')
+            col -= 1
+
+        while dep > 0:
+            aligned_seq_1_inverse.append('-')
+            aligned_seq_2_inverse.append('-')
+            aligned_seq_3_inverse.append(t[dep - 1])
+            dep -= 1
+        
+        return ''.join(aligned_seq_1_inverse[::-1]), ''.join(aligned_seq_2_inverse[::-1]), ''.join(aligned_seq_3_inverse[::-1])
 
 if __name__ == "__main__":
     
@@ -72,5 +183,4 @@ if __name__ == "__main__":
     print(alignment.global_alignment_score)
     print(alignment.aligned_seq_1)
     print(alignment.aligned_seq_2)
-    print(alignment.aligned_seq_2)
-    
+    print(alignment.aligned_seq_3)
