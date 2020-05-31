2ca5658f5bd57caf01a88dcaa9948505800af1eb
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-30 19:14:13 -0700

    #184 is completed

--- 06-dynamic-programming-applications-in-machine-learning-and-genomics/3-hidden-markov-models-for-sequence-alignment-1/outcome_likelihood.py
+++ 06-dynamic-programming-applications-in-machine-learning-and-genomics/3-hidden-markov-models-for-sequence-alignment-1/outcome_likelihood.py
@@ -1,8 +1,23 @@
 import sys
 
-def outcome_likelihood(x,sigma,states,transition,emission):
+def outcome_likelihood(x, sigma, states, transition, emission):
     
-    return 0
+    # The transitions from the initial state occur with equal probability: Prob(P0 = Si) = 1/|states| * Pr(x = x0 | P0)
+    M = [ [ emission[ states[s] ][ x[0] ]/len(states) if p == 0 else 0 for p in range(len(x))] for s in range(len(states)) ]
+        
+    for p in range(len(x) - 1):
+        next_x = x[p + 1]
+
+        for s in range(len(states)):
+            M[s][p + 1] = M[0][p] * transition[ states[0] ][ states[s] ] * emission[ states[s] ][ next_x ]
+
+        for s in range(1, len(states)):
+            for next_s in range(len(states)):
+                M[next_s][p + 1] += M[s][p] * transition[ states[s] ][ states[next_s] ] * emission[ states[next_s] ][ next_x ]
+    prob_x = 0
+    for s in range(len(states)):
+        prob_x += M[s][len(x) - 1]
+    return prob_x
 
 if __name__ == "__main__":
     x = sys.stdin.readline().strip()
