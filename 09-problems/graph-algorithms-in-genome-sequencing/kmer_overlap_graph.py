7bc768db8fedf5f345f11bcd2376ae72020cfbd8
Hamid Gasmi <mamid1706@hotmail.fr>
2020-05-31 03:23:34 -0700

    #185 is started

---
+++ 09-problems/graph-algorithms-in-genome-sequencing/kmer_overlap_graph.py
@@ -0,0 +1,82 @@
+import sys
+
+class Overlap_Graph:
+    def __init__(self, patterns):
+
+        assert(len(patterns) > 0)
+        self._pattern_size = len(patterns[0])
+        self._build_adjacency_list(patterns)
+
+    def _build_adjacency_list(self, patterns):
+        self.nodes = []
+        self.adjacency_list = []
+        self.nodes_ids = dict()
+        prefixes = dict()
+        suffixes = dict()
+        
+        for pattern in patterns:
+            assert(len(pattern) == self._pattern_size)
+            
+            node_id, is_new_node = self.get_node_id(pattern)
+            prefix = pattern[:self._pattern_size - 1]
+            suffix = pattern[1:]
+            #print("pattern, prefix, suffix: ", pattern, prefix, suffix)
+            if is_new_node:
+                if not prefix in prefixes:
+                    prefixes[prefix] = []
+                prefixes[prefix].append(node_id)
+
+                if not suffix in suffixes:
+                    suffixes[suffix] = []
+                suffixes[suffix].append(node_id)
+
+                if suffix in prefixes: 
+                    self.adjacency_list[node_id].extend(prefixes[suffix])
+                if prefix in suffixes:
+                    for predec_node in suffixes[prefix]:
+                        self.adjacency_list[predec_node].append(node_id)
+
+        for a in self.adjacency_list:
+            print(a)
+
+    def get_node_id(self, pattern):
+
+        is_new_node = False
+        if pattern in self.nodes:
+            node_id = self.nodes_ids[pattern]
+            is_new_node = False
+        else:
+            is_new_node = True
+            node_id = len(self.nodes)
+            self.nodes.append(pattern)
+            self.adjacency_list.append([])
+            self.nodes_ids[pattern] = node_id
+        
+        return node_id, is_new_node     
+    
+    def print_adjacency_list(self):
+        
+        result_list = []
+        print(self.nodes)
+        for node in range(len(self.nodes)):
+            if len(self.adjacency_list[node]) == 0:
+                continue
+            print(self.nodes[node])
+
+            node_adjacents = [ self.nodes[node] ]
+            node_adjacents.append('->')
+            for a in range(len(self.adjacency_list[node])):
+                adjacent_node_id = self.adjacency_list[node][a]
+                node_adjacents.append(self.nodes[adjacent_node_id])
+                if a < len(self.adjacency_list[node]) - 1:
+                    node_adjacents.append(',')
+            result_list.append(''.join(node_adjacents))
+
+        return '\n'.join(result_list)
+        
+if __name__ == "__main__":
+    patterns = sys.stdin.read().strip().splitlines()
+    
+    overlap_graph = Overlap_Graph(patterns)
+
+    print(overlap_graph.print_adjacency_list())
