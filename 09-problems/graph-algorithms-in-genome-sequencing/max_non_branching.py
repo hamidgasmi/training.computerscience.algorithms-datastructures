b4e094c5370d67235468b5bcab8644e1f3d4daeb
Hamid Gasmi <mamid1706@hotmail.fr>
2020-06-03 16:37:25 -0700

    #194 is completed

--- 09-problems/graph-algorithms-in-genome-sequencing/max_non_branching.py
+++ 09-problems/graph-algorithms-in-genome-sequencing/max_non_branching.py
@@ -1,18 +1,16 @@
 import sys
-from collections import namedtuple
-
-Node_Degree = namedtuple('Node_Degree', ['vertex_no', 'in_degree', 'out_degree'])
 
 class Graph:
     def __init__(self, edges):
         
         self._build_graph(edges)
+        self._compute_indgrees()
     
     def _build_graph(self, edges):
         
         self.nodes = []
         self.adjacency_list = []
-        
+                
         node_label_dict = dict()
         for edge in edges:    
             (start_vertex_label, end_vertices_labels) = edge.split(' -> ')
@@ -30,23 +28,83 @@ class Graph:
             node_no = len(self.nodes)
             self.nodes.append(label)
             self.adjacency_list.append([])
-            self.reversed_adjacency_list.append([])
             node_label_dict[label] = node_no
 
         return node_no
 
-    def compute_indgrees(self):
-
-        # 1. Compute in_degree for each vertex
+    def _compute_indgrees(self):
+        
         self.in_degree = [0 for _ in range(len(self.adjacency_list))]
         for u in range(len(self.adjacency_list)):
             for v in self.adjacency_list[u]:
                 self.in_degree[v] += 1        
     
+    def find_isolated_cycles(self, maximal_non_branching_paths, maximal_non_branching_nodes):
+
+        for v in range(len(self.nodes)):
+            if v in maximal_non_branching_nodes:
+                continue
+
+            v_out_degree = len(self.adjacency_list[v])
+            if not (self.in_degree[v] == v_out_degree == 1):
+                continue
+            
+            start = v
+
+            non_branching_path = [self.nodes[v]]
+            maximal_non_branching_nodes.add(v)
+            
+            v = self.adjacency_list[v][0]
+            v_out_degree = len(self.adjacency_list[v])
+            while self.in_degree[v] == v_out_degree == 1 and v != start:
+
+                non_branching_path.append(self.nodes[v])
+                maximal_non_branching_nodes.add(v)
+                
+                v = self.adjacency_list[v][0]
+                v_out_degree = len(self.adjacency_list[v])
+
+            if v == start:
+                non_branching_path.append(self.nodes[v])
+                maximal_non_branching_paths.append(non_branching_path)
+
     def find_maximal_non_branching_paths(self):
-        
-        
-        return ""
+
+        non_branching_path_nodes = set()
+
+        paths = []
+        for u in range(len(self.nodes)):
+            
+            u_out_degree = len(self.adjacency_list[u])
+            if not (self.in_degree[u] == u_out_degree == 1):
+                
+                if u_out_degree > 0:
+                    
+                    non_branching_path_nodes.add(u)
+
+                    for v in self.adjacency_list[u]:
+                        
+                        non_branching_path = [self.nodes[u]]
+                        
+                        v_out_degree = len(self.adjacency_list[v])
+                        while self.in_degree[v] == v_out_degree == 1:
+                            
+                            non_branching_path.append(self.nodes[v])
+                            if v in non_branching_path_nodes:
+                                non_branching_path_nodes.add(v)
+                            
+                            v = self.adjacency_list[v][0]
+                            v_out_degree = len(self.adjacency_list[v])
+
+                        non_branching_path.append(self.nodes[v])
+                        if v in non_branching_path_nodes:
+                                non_branching_path_nodes.add(v)
+                        paths.append(non_branching_path)
+
+        # Find isolated cycles
+        self.find_isolated_cycles(paths, non_branching_path_nodes)
+                
+        return '\n'.join( ['->'.join(paths[i]) for i in range(len(paths))])
 
 
 if __name__ == "__main__":
