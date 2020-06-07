80a4caba24b37342fb0dea602d53846c7ff1e8ba
Hamid Gasmi <mamid1706@hotmail.fr>
2020-06-06 20:46:00 -0700

    #198 is completed

--- 09-problems/graph-algorithms-in-genome-sequencing/detect_bubble.py
+++ 09-problems/graph-algorithms-in-genome-sequencing/detect_bubble.py
@@ -1,5 +1,5 @@
 import sys
-import math
+from queue import Queue
 
 class De_Bruijn_Graph:
     def __init__(self, k, t, reads):
@@ -24,14 +24,17 @@ class De_Bruijn_Graph:
 
                 prefix_node_no = self._get_node_no(read[0:k-1], node_kmer_no_dict) if i == 0 else suffix_node_no
                 suffix_node_no = self._get_node_no(read[i+1:i+k], node_kmer_no_dict)
-                if prefix_node_no == suffix_node_no:
+                
+                if suffix_node_no in self.adjacency_list[prefix_node_no]:
                     continue
 
-                if not suffix_node_no in self.adjacency_list[prefix_node_no]:
-                    self.adjacency_list[prefix_node_no].add(suffix_node_no)
+                self.adjacency_list[prefix_node_no].add(suffix_node_no)
 
-                    self.out_degree[prefix_node_no] += 1
-                    self.in_degree[suffix_node_no] += 1
+                if prefix_node_no == suffix_node_no:
+                    continue
+                
+                self.out_degree[prefix_node_no] += 1
+                self.in_degree[suffix_node_no] += 1
             
     def _get_node_no(self, label, node_label_no_dict):
         
@@ -48,139 +51,91 @@ class De_Bruijn_Graph:
 
         return node_no
     
-    def find_isolated_cycles(self, maximal_non_branching_paths, maximal_non_branching_nodes):
-
-        for v in range(len(self.nodes)):
-            if v in maximal_non_branching_nodes:
-                continue
-
-            v_out_degree = len(self.adjacency_list[v])
-            if not (self.in_degree[v] == v_out_degree == 1):
-                continue
-            
-            start = v
-
-            non_branching_path = [v]
-            maximal_non_branching_nodes.add(v)
-            
-            v = next(iter(self.adjacency_list[v]))
-            v_out_degree = len(self.adjacency_list[v])
-            while self.in_degree[v] == v_out_degree == 1 and v != start:
-
-                non_branching_path.append(v)
-                maximal_non_branching_nodes.add(v)
-                
-                v = next(iter(self.adjacency_list[v]))
-                v_out_degree = len(self.adjacency_list[v])
-
-            if v == start:
-                non_branching_path.append(v)
-                maximal_non_branching_paths.append(non_branching_path)
-
-    def find_maximal_non_branching_paths(self):
+    def short_paths_bfs(self, u, short_paths, bubble_treshold):
         
-        paths = dict()
-        #non_branching_path_starts = dict()
+        # path as an array: to get path last element in O(1) time
+        q_paths = Queue()
+        # path as a set(): to check if a node is in the path in O(1)
+        q_paths_set = Queue()
 
+        q_paths.put([u])
+        q_paths_set.put({u})
+
+        while not q_paths.empty():
+            
+            path = q_paths.get()
+            path_set = q_paths_set.get()
+
+            path_tail_node = path[len(path) - 1]
+
+            if (path_tail_node != u) and (not path_tail_node in short_paths):
+                    short_paths[ path_tail_node ] = []
+            
+            if len(path) > 1:
+                short_paths[ path_tail_node ].append(path_set.copy())
+                short_paths[ path_tail_node ][ len(short_paths[ path_tail_node ]) - 1 ].remove(path[0])
+                short_paths[ path_tail_node ][ len(short_paths[ path_tail_node ]) - 1 ].remove(path_tail_node)
+
+            if (len(path) - 1) >= bubble_treshold:
+                continue
+            
+            for v in self.adjacency_list[path_tail_node]:
+                
+                if v in path_set:
+                    continue
+                
+                # There is only 1 path going to a node which in_degree = out_degree = 1
+                # It's not necessary to enqueue them
+                w = v
+                node_path = []
+                node_path_set = set()
+                while self.in_degree[w] == self.out_degree[w] == 1:
+                    node_path.append(w)
+                    node_path_set.add(w)
+
+                    w = next(iter(self.adjacency_list[w]))
+                    
+                    if (len(path) + len(node_path) -1) >= bubble_treshold:
+                        break
+                node_path.append(w)
+                node_path_set.add(w)
+                
+                # void cycles
+                if w in path_set:
+                    continue
+                
+                # dead-end node: there is only 1 path passing by
+                if self.out_degree[w] == 0 and self.in_degree[w] == 1:
+                    continue
+                
+                # Long path
+                if (len(path) + len(node_path) - 1) > bubble_treshold:
+                    continue
+                
+                q_paths_set.put(path_set.union(node_path_set))
+                q_paths.put(path + node_path)
+                
+    def detect_bubbles(self):
+                
+        bubble_count = 0
         for u in range(len(self.nodes)):
             
-            if not (self.in_degree[u] == self.out_degree[u] == 1):
+            if self.out_degree[u] > 1:
                 
-                # Bubble's start node outdegre > 1
-                if self.out_degree[u] > 0:
-                    
-                    for v in self.adjacency_list[u]:
-                        
-                        non_branching_path = [ u ]
-                                                
-                        while self.in_degree[v] == self.out_degree[v] == 1:
-                            
-                            non_branching_path.append(v)
-                            
-                            v = next(iter(self.adjacency_list[v]))
-                        
-                        non_branching_path.append(v)
-
-                        # Bubble's end node indegre > 1
-                        if self.in_degree[v] == 1:
-                            continue
-                        
-                        if len(non_branching_path) - 1 > self._bubble_treshold:
-                            continue
-                        
-                        if not u in paths:
-                            paths[u] = []
-                        
-                        #non_branching_path_starts[u].add(len(paths))
-                        paths[u].append(non_branching_path)
-
-        return paths
-
-    def detect_bubbles(self):
-        
-        max_non_branching_paths = self.find_maximal_non_branching_paths()
-
-        paths_queue = []
-        for node_paths in max_non_branching_paths.values():
-            
-            for node_path in node_paths:
-
-                paths_queue.append(node_path)
-
-        bubble_start_end = dict()
-        while len(paths_queue) != 0:
-            path = paths_queue.pop(0)
-
-            path_start_node = path[0]
-            if not path_start_node in bubble_start_end:
-                bubble_start_end[ path_start_node ] = dict()
-
-            path_end_node  = path[len(path) - 1]
-            if not path_end_node in bubble_start_end[ path_start_node ]:
-                bubble_start_end[ path_start_node ][ path_end_node ] = 0
-            bubble_start_end[ path_start_node ][ path_end_node ] += 1
-            
-            if not path_end_node in max_non_branching_paths:
-                continue
-
-            for adjacent_path in max_non_branching_paths[path_end_node]:
-                if len(path) + len(adjacent_path) - 1 > self._bubble_treshold:
-                    continue
-                if adjacent_path[len(adjacent_path) - 1] in path:
-                    continue
+                # short_paths contains all paths starting from u
+                #    dict key = end path node (v)
+                #    dict value: a list of all short paths from u to v (dict key)
+                #              : u and v aren't included in path list
+                #              : Each path is a set of nodes
+                short_paths = dict()
+                self.short_paths_bfs(u, short_paths, self._bubble_treshold)
                 
-                paths_queue.append(path + adjacent_path[1:])
-        
-        #print(bubble_start_end)
-        bubble_count = 0
-        #print(bubble_start_end.values())
-        for candidate_bubble in bubble_start_end.values():
-            
-            paths_count = next(iter(candidate_bubble.values()))
-
-            #if path_count > 2, then there are n choose k bubbles n = paths count and k = 2
-            bubble_count += 0 if paths_count < 2 else ((math.factorial(paths_count) // math.factorial(paths_count - 2)) // 2)
+                for paths in short_paths.values():
+                    for i in range(len(paths)):
+                        for j in range(i + 1, len(paths)):
+                            bubble_count += 1 if paths[i].isdisjoint(paths[j]) else 0
 
         return bubble_count
-
-    def str_adjacency_list(self):
-
-        result_list = []
-        for node in range(len(self.nodes)):
-            if len(self.adjacency_list[node]) == 0:
-                continue
-            
-            node_adjacents = [ self.nodes[node] ]
-            node_adjacents.append(' -> ')
-            
-            for adjacent_node in self.adjacency_list[node]:
-                
-                node_adjacents.append(self.nodes[adjacent_node])
-                node_adjacents.append(',')
-            
-            result_list.append(''.join(node_adjacents))
-
-        return '\n'.join(result_list)
 
 if __name__ == "__main__":
 
@@ -189,7 +144,5 @@ if __name__ == "__main__":
     reads = sys.stdin.read().strip().splitlines()
     
     de_bruijn_graph = De_Bruijn_Graph(k, t, reads)
-
-    #print(de_bruijn_graph.str_adjacency_list())
-
-    print(de_bruijn_graph.detect_bubbles())    
+    
+    print(de_bruijn_graph.detect_bubbles())
