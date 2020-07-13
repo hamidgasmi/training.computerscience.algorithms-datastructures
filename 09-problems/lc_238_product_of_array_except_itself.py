6b7665918136757577195cfa06303df806d9813b
Hamid Gasmi <mamid1706@hotmail.fr>
2020-07-12 19:39:49 -0700

    #235 is completed

--- 09-problems/lc_238_product_of_array_except_itself.py
+++ 09-problems/lc_238_product_of_array_except_itself.py
@@ -4,18 +4,20 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
         len_nums = len(nums)
+
+        assert(len_nums > 1)
         
         product_nums = [1 for _ in range(len_nums)]
         
-        product = 1
+        left = 1
         for i in range(1, len_nums):
-            product *= nums[i - 1]
-            product_nums[i] *= product
+            left *= nums[i - 1]
+            product_nums.append(left)
             
-        product = 1
+        right = 1
         for i in range(len_nums - 2, -1, -1):
-            product *= nums[i + 1]
-            product_nums[i] *= product
+            right *= nums[i + 1]
+            product_nums[i] *= right
         
         return product_nums
         
