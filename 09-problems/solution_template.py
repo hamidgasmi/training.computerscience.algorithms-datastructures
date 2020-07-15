f9688c6319a24a0814dda8bff05a895d05701ded
Hamid Gasmi <mamid1706@hotmail.fr>
2020-07-15 15:30:49 -0700

    Code templated is added

---
+++ 09-problems/solution_template.py
@@ -0,0 +1,24 @@
+"""
+    1. Problem Summary / Clarifications:
+
+
+    2. Intuition:
+
+
+    3. Implementation
+
+
+    4. Tests:
+
+
+    5. Analysis:
+       Time Complexity:
+       Space Compexity:
+
+
+    6. Optimization:
+
+
+"""
+class Solution:
+    pass
