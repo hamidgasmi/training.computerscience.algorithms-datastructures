6f43621e987c27a04476fb5eef60a495250e4aec
Hamid Gasmi <mamid1706@hotmail.fr>
2020-07-14 22:11:57 -0700

    Binary tree implementation is started

--- 10-algo-ds-implementations/tree_binary_tree_node.py
+++ 10-algo-ds-implementations/tree_binary_tree_node.py
@@ -6,9 +6,17 @@ class BinaryTreeNode:
         self.left = left
         self.right = right
 
+    def __repr__(self):
+        
+        return (f"val: {self.val}, left: {None if self.left is None else self.left.val}"
+                            + f", right : {None if self.right is None else self.right.val}")
+
 class BinaryTreeNodeParent(BinaryTreeNode):
     def __init__(self, val, parent:BinaryTreeNodeParent=None, left:BinaryTreeNodeParent=None, rigth:BinaryTreeNodeParent=None):
         
         super().__init__(val, left, rigth)
         self.parent = parent
-        
+    
+    def __repr__(self):
+
+        return ( super().__repr() + f", parent : {None if self.parent is None else self.parent.val}")
