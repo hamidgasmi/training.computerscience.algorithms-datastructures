db3ff6e7f9ad2ae179a8539c4fb9492e63c09f9d
Hamid Gasmi <mamid1706@hotmail.fr>
2020-07-17 17:18:38 -0700

    #238 is completed

---
+++ 09-problems/lc_743_network_delay_time.py
@@ -0,0 +1,34 @@
+import math
+import heapq
+import collections
+   
+class Solution:
+    def network_delay_time(self, times: List[List[int]], N: int, K: int) -> int:
+        
+        adjacency_list = dict()
+        for u, v, w in times:
+            if not u in adjacency_list:
+                adjacency_list[u] = []
+            adjacency_list[u].append((v, w))
+        
+        distances = {key:math.inf for key in range(1, N + 1)}
+        distances[K] = 0
+        
+        hq = [(distances[K], K)]
+        while hq:
+            (distance, v) = heapq.heappop(hq)
+            
+            if not v in adjacency_list:
+                continue
+            
+            for (a,w) in adjacency_list[v]:
+                candidate_distance = distances[v] + w
+                
+                if distances[a] > candidate_distance:
+                    distances[a] = candidate_distance
+                    heapq.heappush(hq, (distances[a], a))
+        
+        max_distance = max(distances.values())
+        
+        return -1 if max_distance == math.inf else max_distance
+        
