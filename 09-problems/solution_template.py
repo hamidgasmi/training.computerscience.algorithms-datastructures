80e5ad2442a0c852eb7e77b9b0951822ada489a8
Hamid Gasmi <mamid1706@hotmail.fr>
2020-07-17 17:17:33 -0700

    Solution class template is added

--- 09-problems/solution_template.py
+++ 09-problems/solution_template.py
@@ -1,23 +1,15 @@
 """
-    1. Problem Summary / Clarifications:
-
-
+    1. Problem Summary / Clarifications / TDD:
+    
     2. Intuition:
-
+        - Naive:
+        - We could do better:
 
     3. Implementation
-
-
     4. Tests:
-
-
     5. Analysis:
        Time Complexity:
        Space Compexity:
-
-
-    6. Optimization:
-
 
 """
 class Solution:
