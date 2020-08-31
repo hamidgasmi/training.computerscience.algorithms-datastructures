2b635a28675f13ff48131ce6be4544902e3ded6c
Hamid Gasmi <mamid1706@hotmail.fr>
2020-08-30 18:33:21 -0700

    #282 Union-Find solution is added

--- 09-problems/lc_952_largest_component_size.py
+++ 09-problems/lc_952_largest_component_size.py
@@ -1,7 +1,79 @@
-class SolutionDisjointSets:
+class DisjointSet:
+
+    def __init__(self, nodes_count: int):     
+        self.rank = [0 for _ in range(nodes_count)]
+        self.parent = [i for i in range(nodes_count)]
+    
+    def find(self, i):
+        # 1. find parent
+        root = i
+        while self.parent[root] != root:
+            root = self.parent[root]
+        
+        # 2. Compress Path
+        while i != root:
+            self.parent[i], i = root, self.parent[i]
+        
+        return root
+    
+    def union(self, i, j):
+        root_i = self.find(i)
+        root_j = self.find(j)
+        
+        if root_i == root_j:
+            return
+        
+        if self.rank[root_i] > self.rank[root_j]:
+            self.parent[root_j] = root_i
+            
+        else:
+            self.parent[root_i] = root_j
+            
+            if self.rank[root_i] == self.rank[root_j]:
+                self.rank[root_j] += 1
+
+class SolutionUnionFind:
+
+    def largest_component_size(self, A: List[int]) -> int:
+                
+        # 1. Compute Factors & Compute Common Disjoint Sets
+        disjoint_set = DisjointSet(max(A) + 1)
+        for n in A:
+            factors = self.get_prime_factors(n)
+            
+            disjoint_set.union(n, factors[0])
+        
+            for i in range(1, len(factors)):
+                disjoint_set.union(factors[i-1], factors[i])
+        
+        # 3. Find the largest component size
+        max_size = 0
+        components_size = dict()
+        for i in range(len(A)):
+            root = disjoint_set.find(A[i])
+            components_size[root] = components_size.get(root, 0) + 1
+            
+            max_size = max(max_size, components_size[root])
+        
+        return max_size
+    
+    def get_prime_factors(self, n):
+        
+        factor = 2
+        prime_factors = []
+        while n >= factor * factor:
+            if n % factor == 0:
+                prime_factors.append(factor)
+                n //= factor
+            else:
+                factor += 1
+        prime_factors.append(n)
+        
+        return prime_factors
 
 class SolutionDFS:
-    def largestComponentSize(self, A: List[int]) -> int:
+    
+    def largest_component_size(self, A: List[int]) -> int:
         
         self.vertices_count = len(A)
         
