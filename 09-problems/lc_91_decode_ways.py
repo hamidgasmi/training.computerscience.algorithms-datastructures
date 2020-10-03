4b88fc50b1d5366ebde69948d90c611a2d4434b0
Hamid Gasmi <mamid1706@hotmail.fr>
2020-10-03 15:56:33 -0700

    #291 is completed with a DP buttom-up approach

--- 09-problems/lc_91_decode_ways.py
+++ 09-problems/lc_91_decode_ways.py
@@ -7,7 +7,7 @@ class SolutionRecursive:
     Complexity Analysis:
         Time Complexity: O(N)
         Space Complexity: O(N)
-    
+
     """
     def num_decodings(self, s: str) -> int:
 
@@ -33,3 +33,32 @@ class SolutionRecursive:
         memo[idx] = decoding_count
         
         return decoding_count
+
+class SolutionDP:
+    """
+    Intuition:
+        - Dynamic Programming
+        - dp[0] = 0
+        - dp[1] = 0 if s[0] = '0' else 1
+        - dp[i] = dp[i - 2] if s[i - 1] = '0' and s[i - 2] in ('1', '2')
+        - dp[i] = dp[i - 1] if s[i - 1] != '0'
+                + dp[i - 2] if <= s[i - 2: i] < 27)
+    
+    Complexity Analysis:
+        Time Complexity: O(N)
+        Space Complexity: O(N)
+
+    """
+    def num_decodings(self, s: str) -> int:
+        len_dp = len(s) + 1
+        
+        dp = [1, 0 if s[0] == '0' else 1]
+        for i in range(2, len_dp):
+            
+            s_curr_idx = i - 1
+            
+            dp.append(0)
+            dp[i] = 0 if s[s_curr_idx] == '0' else dp[i - 1]
+            dp[i] += dp[i - 2] if 9 < int(s[s_curr_idx - 1: s_curr_idx + 1]) < 27 else 0              
+        
+        return dp[len_dp - 1]
