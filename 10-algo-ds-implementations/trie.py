8817ff525f9b5419ec616c964c51b9dce0a60e41
Hamid Gasmi <mamid1706@hotmail.fr>
2020-10-15 12:50:41 -0700

    Trie data-structure is added

---
+++ 10-algo-ds-implementations/trie.py
@@ -0,0 +1,32 @@
+class Trie:
+    def __init__(self):
+        self.root = {}
+
+    # Time Complexity: O(|word|)
+    # Space Complexity: O(|word|)
+    def add(self, word):
+        
+        node = self.root
+        for c in word:
+            if c not in node:
+                node.add(c)
+            node = node[c]
+        if '$' not in node:
+            node['$'] = None
+
+    # Time Complexity: O(|word|)
+    # Space Complexity: O(1)
+    def search(self, word):
+
+        node = self.root
+        for c in word:
+            if c not in node:
+                return False
+
+            node = node[c]
+        
+        return True if  '$' in node else False
+
+                
+
+            
