74a36de3a1a4ff912be32bba4df9153aed17c132
Hamid Gasmi <mamid1706@hotmail.fr>
2020-10-26 05:48:55 -0700

    Solution template is updated

--- 09-problems/solution_template.py
+++ 09-problems/solution_template.py
@@ -1,6 +1,6 @@
 """
     1. Problem Summary / Clarifications / TDD:
-    
+
 """
 
 """
@@ -12,10 +12,19 @@
 
     3. Implementation
     4. Tests:
-    5. Analysis:
+    5. Complexity Analysis:
        Time Complexity:
        Space Compexity:
 
 """
 class Solution:
-    pass
+    def __init__(self):
+        pass
+
+    def solve(self):
+        pass
+
+if __name__ == "__main__":
+
+    s = Solution()
+    print(s.solve())
