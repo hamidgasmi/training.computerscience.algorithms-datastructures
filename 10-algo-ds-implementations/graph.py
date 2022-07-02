f4de10291df32287791e4533bd57f20c30972ecc
Hamid Gasmi <mamid1706@hotmail.fr>
2022-07-02 13:19:30 -0700

    #297: implement graph with adjacency list in a separate class

--- 10-algo-ds-implementations/graph.py
+++ 10-algo-ds-implementations/graph.py
@@ -1,11 +1,28 @@
+from typing import List
+from collections import namedtuple
 
+'''
+    Build Adjacency List
+    Input: Eges List
+    Output: 
+        - Graph implemented with an adjacency list
+    
+    Time and Space Complexity:
+        - Time Complexity: O(|E|) 
+        - Space Complexity: O(|V| + |E|)
+    
+'''
 
-class Weighted_Edge:
-    def __init__(self, source: int, sink: int, weight: int):
-        self.source = source
-        self.sink = sink
-        self.weight = weight
+Edge = namedtuple('Edge', ['source', 'sink', 'weight'])
 
-class Positively_Weighted_Edge(Weighted_Edge):
-    def __init__(self, source: int, sink: int, weight: int):
-        assert(weight >= 0)
+class Graph_Adjacency_List:
+    def __init__(self, vertices_count: int, edges: List[Edge]):
+        self.vertices_count = vertices_count
+        self.adjacency_list = self.__build_adjacency_list(edges) # O(|E|)
+    
+    def __build_adjacency_list(self, edges: List[Edge]) -> List[List[Edge]]:
+        adjacency_list = [ [] for _ in range(self.__vertices_count) ]
+        for edge in edges:
+            adjacency_list[edge.source] = edge
+        
+        return adjacency_list
