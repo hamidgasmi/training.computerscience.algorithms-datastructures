7bd961942598cb8e7cc42f5fd11f32329c0f7bee
Hamid Gasmi <mamid1706@hotmail.fr>
2022-07-03 10:51:03 -0700

    #297: use fastest_path_base class

--- 10-algo-ds-implementations/graph_fastest_path_dijkstra_array.py
+++ 10-algo-ds-implementations/graph_fastest_path_dijkstra_array.py
@@ -1,6 +1,3 @@
-from typing import List
-from graph import Graph_Adjacency_List
-
 '''
     Dijkstra's Algorithm
     Input: Weighted Graph: G(V, E, W), s ∈ V
@@ -19,38 +16,19 @@ from graph import Graph_Adjacency_List
         - Space Complexity: O(|V|)
             - S(Parent list) + S(distance list) + S(visited nodes set) = O(|V| + |V| + |V|) = O(|V|)
     
+    Implementation Assumptions:
+        - Directed graph
+        - Edges number starts from 0 (zero-based numbering)
+        - Edges list is valid
+            - 0 <= edge.source, edge.sink < vertices_count for any edge in edges list
+            - It does not have duplicates
+    
 '''
+from typing import List
+from graph_fastest_path_base import Fastest_Path_Base
 
-Candidate = namedtuple('Candidate', ['distance', 'node'])
-
-class Dijkstra_Heap_Queue:
-    def __init__(self, graph: Graph_Adjacency_List, s: int):
-        self.__max_distance = 10**7
+class Dijkstra_Heap_Queue(Fastest_Path_Base):
         
-        self.__graph = graph
-
-        self.__path_source_node = s
-        self.__parent = []
-        self.__distance = []
-    
-    def fastest_distance_to(self, dest: int) -> int:
-        if len(self.__distance) == 0:
-            self.__compute_fastest_path()
-        
-        return self.__distance[dest]
-
-    def fastest_path_to(self, dest: int) -> List[int]:
-        if len(self.__distance) == 0:
-            self.__compute_fastest_path()
-        
-        v = dest
-        reversed_path = []
-        while v != -1:
-            reversed_path.append(v)
-            v = self.__parent[v]
-        
-        return reversed_path[::-1]
-    
     # O(|V|)
     def __get_closest_node(self, visited_nodes: set) -> int:
         closest_node = -1
@@ -61,7 +39,8 @@ class Dijkstra_Heap_Queue:
                 min_distance = self.__distance[candidate]
         
         return closest_node
-
+    
+    # O(|V|^2)
     def __compute_fastest_path (self):
         # Initialization
         self.__parent = [ -1 for _ in range(self.__graph.vertices_count) ]                       # O(|V|)
@@ -84,5 +63,3 @@ class Dijkstra_Heap_Queue:
                     self.__distance[ adjacent ] = candidate_distance
             
             closest_node = self.__get_closest_node(visted_nodes)   # O(|V|)
-
-        
--- 10-algo-ds-implementations/graph_fastest_path_dijkstra_heapq.py
+++ 10-algo-ds-implementations/graph_fastest_path_dijkstra_heapq.py
@@ -1,7 +1,3 @@
-import heapq
-from typing import List
-from graph import Graph_Adjacency_List
-
 '''
     Dijkstra's Algorithm
     Input: Weighted Graph: G(V, E, W), s ∈ V
@@ -16,7 +12,7 @@ from graph import Graph_Adjacency_List
         - Time Complexity: O(|V| + |E|log|E|)
             - T(Initialization) + T(Compute Min Distance) = O(|V|) + P(|E|*log|E|)
             - Dense graph (|E| = O(|V|^2)) --> T = O(|V| + |V|^2 * log|V|^2) = O(|V|^2 * log|V|)
-            - Sparce graph (|E| ~ |V|)     --> T = O(|V| + |V|log|V|) = O(|V|log|V|) 
+            - Sparce graph (|E| ~ |V|)     --> T = O(|V| + |V|log|V|) = O(|V|log|V|)
         - Space Complexity: O(|V| + |E|)
             - S(Parent list) + S(distance list) + S(Heap queue) = O(|V| + |V| + |E|) = O(|V| + |E|)
     
@@ -28,35 +24,12 @@ from graph import Graph_Adjacency_List
             - It does not have duplicates
     
 '''
+import heapq
+from typing import List
+from graph_fastest_path_base import Fastest_Path_Base
 
-class Dijkstra_Heap_Queue:
-    def __init__(self, graph: Graph_Adjacency_List, s: int):
-        self.__max_distance = 10**7
-        
-        self.__graph = graph
+class Dijkstra_Heap_Queue(Fastest_Path_Base):
 
-        self.__path_source_node = s
-        self.__parent = []
-        self.__distance = []
-    
-    def fastest_distance_to(self, dest: int) -> int:
-        if len(self.__distance) == 0:
-            self.__compute_fastest_path()
-        
-        return self.__distance[dest]
-
-    def fastest_path_to(self, dest: int) -> List[int]:
-        if len(self.__distance) == 0:
-            self.__compute_fastest_path()
-        
-        v = dest
-        reversed_path = []
-        while v != -1:
-            reversed_path.append(v)
-            v = self.__parent[v]
-        
-        return reversed_path[::-1]
-    
     def __compute_fastest_path (self):
         # Initialization
         self.__parent = [ -1 for _ in range(self.__graph.vertices_count) ]                       # O(|V|)
